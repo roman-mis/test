ee20cc6e72fe83fe1e7e8d97d636701c8eaa16b6
Administrator <wangkang89@hotmail.com>
2015-01-28 12:29:53 +0800

    rename variables in candidate register page

--- app/scripts/controllers/register/step1.js
+++ app/scripts/controllers/register/step1.js
@@ -11,19 +11,4 @@ angular.module('origApp.controllers')
         .controller('RegisterStep1Controller', function($scope, candidate) {
             $scope.candidate = candidate;
             $scope.titleary = ['Mr', 'Mrs', 'Ms'];
-            console.log(candidate);
-            //updated
-            // $scope.saveEdition = function() {                
-            //     candidate.setAttributes({
-            //         title: $scope.candidate.title,
-            //         firstName: $scope.candidate.firstName,
-            //         lastName: $scope.candidate.lastName,
-            //         emailAddress: $scope.candidate.emailAddress,
-            //         contactNumber: $scope.candidate.contactNumber,
-            //         niNumber: $scope.candidate.niNumber,
-            //         dateOfBirthday: $scope.candidate.dateOfBirthday
-            //     })
-            //     $scope.$parent.confirm.ContactDetail1 = true;
-            //     return true;
-            // };
-        })
+        });
