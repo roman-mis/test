3cdd44477522d4f1c64595f65a78ac5c09e19f93
boojaka <boojaka@gmail.com>
2015-02-03 15:00:18 +0200

    checking gmail.com spec added

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -10,7 +10,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   suites: {
-    main:['./spec/login.js'],
+    main:['./spec/reg.js','./spec/check-inbox.js'],
     reg: './spec/reg.js',
     login: './spec/login.js',
     candidates: ['./spec/login.js','./spec/candidates.js'],
