85638e3c911275bef7c0a4b975b5463f39faf870
boojaka <boojaka@gmail.com>
2015-02-03 15:40:29 +0200

    minor commmit for test

--- Gruntfile.js
+++ Gruntfile.js
@@ -447,7 +447,7 @@ module.exports = function (grunt) {
       options: {
         // Task-specific options go here.
       },
-      test: {},
+      test: {}
     },
     protractor: {
       options: {
