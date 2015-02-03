81b099b18c8e6dd82f717b745928e9bb79291af3
Administrator <wangkang89@hotmail.com>
2015-02-04 00:56:43 +0800

    update UK post code validation rule

--- app/scripts/services/validation_helper.js
+++ app/scripts/services/validation_helper.js
@@ -6,7 +6,8 @@ angular.module('origApp.services')
             isValidPostCode: function(value) {
               var valid = true;
               if (angular.isDefined(value) && value.length > 0) {
-                valid = /[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}/gi.test(value);
+                //valid = /[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}/gi.test(value);
+                valid = /^([A-Za-z]{1,2}[0-9]{1,2}[A-Za-z]?[ ]?)([0-9]{1}[A-Za-z]{2})$/gi.test(value);
               }
               return valid;
             }
