6967659bf20221f29673f5158862c463d2436c8e
Ishwor Mali <ishanbuddy@gmail.com>
2015-02-04 01:06:43 +0545

    unit test for getTaskDetails function in task controller. refactored taskservice.js for unit testing.

--- server/services/taskservice.js
+++ server/services/taskservice.js
@@ -1,65 +1,65 @@
-// 'use strict';
+ 'use strict';
 
-var db = require('../models');
-var Q=require('q');
-// var Promise=require('promise');
-var _=require('lodash');
-var mailer=require('../mailing/mailer');
-var uuid = require('node-uuid');
-var url=require('url');
-var service={};
-var utils=require('../utils/utils');
-var awsservice=require('../services/awsservice');
-var candidatecommonservice=require(__dirname+'/candidatecommonservice');
 
-service.getTaskDetails=function(userId){
-	var q = db.Task.find({user: userId, taskCategory: 'TASK'});
-	return Q.nfcall(q.exec.bind(q));
-}
+module.exports=function(dbs){
 
-service.postTaskDetails = function(userId, taskDetails, historyDetails){
-	return Q.Promise(function(resolve,reject){
-		candidatecommonservice.getUser(userId)
-		   .then(function(user){
-		   		if(user){
-	   				var taskModel, historyModel;
-					taskModel = new db.Task(taskDetails);
-   					historyModel = new db.History(historyDetails);
-   					return Q.all([Q.nfcall(taskModel.save.bind(taskModel)), Q.nfcall(historyModel.save.bind(historyModel))])
-   					.then(function(result){
-						resolve({object:taskModel});						
-					}, reject);
-	   			}
-		   		else{
-		   			reject({name:'NotFound', message:'No User Found'});
-		   		}
-		},reject);
-	});
-}
+	var db = dbs;
+	var Q=require('q');
+	// var Promise=require('promise');
+	
+	var candidatecommonservice=require(__dirname+'/candidatecommonservice');
 
-service.getCalllogDetails=function(userId){
-	var q = db.Task.find({user: userId, taskCategory: 'CALL_LOG'});
-	return Q.nfcall(q.exec.bind(q));
-}
+	var service={};
+	
+	service.getTaskDetails=function(userId){
+		var q = db.Task.find({user: userId, taskCategory: 'TASK'});
+		return Q.nfcall(q.exec.bind(q));
+	};
 
-service.postCalllogDetails = function(userId, taskDetails, historyDetails){
-	return Q.Promise(function(resolve,reject){
-		candidatecommonservice.getUser(userId)
-		   .then(function(user){
-		   		if(user){
-	   				var taskModel, historyModel;
-					taskModel = new db.Task(taskDetails);
-   					historyModel = new db.History(historyDetails);
-   					return Q.all([Q.nfcall(taskModel.save.bind(taskModel)), Q.nfcall(historyModel.save.bind(historyModel))])
-   					.then(function(result){
-						resolve({object:taskModel});						
-					}, reject);
-	   			}
-		   		else{
-		   			reject({name:'NotFound', message:'No User Found'});
-		   		}
-		},reject);
-	});
-}
+	service.postTaskDetails = function(userId, taskDetails, historyDetails){
+		return Q.Promise(function(resolve,reject){
+			candidatecommonservice.getUser(userId)
+			   .then(function(user){
+			   		if(user){
+		   				var taskModel, historyModel;
+						taskModel = new db.Task(taskDetails);
+	   					historyModel = new db.History(historyDetails);
+	   					return Q.all([Q.nfcall(taskModel.save.bind(taskModel)), Q.nfcall(historyModel.save.bind(historyModel))])
+	   					.then(function(result){
+							resolve({object:taskModel});						
+						}, reject);
+		   			}
+			   		else{
+			   			reject({name:'NotFound', message:'No User Found'});
+			   		}
+			},reject);
+		});
+	};
 
-module.exports = service;
+	service.getCalllogDetails=function(userId){
+		var q = db.Task.find({user: userId, taskCategory: 'CALL_LOG'});
+		return Q.nfcall(q.exec.bind(q));
+	};
+
+	service.postCalllogDetails = function(userId, taskDetails, historyDetails){
+		return Q.Promise(function(resolve,reject){
+			candidatecommonservice.getUser(userId)
+			   .then(function(user){
+			   		if(user){
+		   				var taskModel, historyModel;
+						taskModel = new db.Task(taskDetails);
+	   					historyModel = new db.History(historyDetails);
+	   					return Q.all([Q.nfcall(taskModel.save.bind(taskModel)), Q.nfcall(historyModel.save.bind(historyModel))])
+	   					.then(function(result){
+							resolve({object:taskModel});						
+						}, reject);
+		   			}
+			   		else{
+			   			reject({name:'NotFound', message:'No User Found'});
+			   		}
+			},reject);
+		});
+	};
+
+	return service;
+};
