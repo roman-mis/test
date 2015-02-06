0a2271789f3d61683920aa4bdbabf5f288641747
Ahmed Hashem <ahashem302@gmail.com>
2015-02-06 14:47:05 +0200

    no message

--- app/scripts/controllers/admin/CubeCtrl.js
+++ app/scripts/controllers/admin/CubeCtrl.js
@@ -1,26 +1,8 @@
 var app = angular.module('origApp.controllers');
 
-app.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-  .state('app.admin.one', {
-    url: '/one',
-    templateUrl: 'views/admin/page1.html'
-  })
-  .state('app.admin.two', {
-    url: '/two',
-    templateUrl: 'views/admin/page2.html'
-  })
-  .state('app.admin.three', {
-    url: '/three',
-    templateUrl: 'views/admin/page3.html'
-  });
-  $urlRouterProvider.otherwise('/one');
-});
-
 app.controller('CubeCtrl', function($scope, $rootScope, $location) {
 
-  $rootScope.breadcrumbs = [{link:'/', text:'Home'},
-                            {link: '/admin', text: 'Admin'}];
+  $rootScope.breadcrumbs = [{link:'/', text:'Home'}, {link: '/admin', text: 'Admin'}];
 
   $scope.go = function(path) {
   $location.path(path);
--- app/scripts/routes/admin.js
+++ app/scripts/routes/admin.js
@@ -1,8 +1,20 @@
 angular.module("origApp").config(function($stateProvider) {
-  $stateProvider
-    .state('app.admin', {
-      url: '/admin',
-      templateUrl: 'views/admin/home.html',
-      controller: 'CubeCtrl'
-    });
+	$stateProvider
+	.state('app.admin', {
+		url: '/admin',
+		templateUrl: 'views/admin/index.html',
+		controller: 'CubeCtrl'
+	})
+	.state('app.admin.home', {
+		url: '/home',
+		templateUrl: 'views/admin/home.html'
+	})
+	.state('app.admin.two', {
+		url: '/two',
+		templateUrl: 'views/admin/page2.html'
+	})
+	.state('app.admin.three', {
+		url: '/three',
+		templateUrl: 'views/admin/page3.html'
+	});
 });
