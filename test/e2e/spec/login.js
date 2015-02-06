5ad0948ae501cfc1d20354006d94e13f302868a0
boojaka <boojaka@gmail.com>
2015-02-06 20:03:19 +0200

    save candidates commit with unresolved bugs

--- test/e2e/spec/login.js
+++ test/e2e/spec/login.js
@@ -1,6 +1,6 @@
-loginData.userName='originemtest+t240444';
+/*loginData.userName='originemtest+t240444';
 loginData.userEmail=loginData.userName+'@yandex.com';
-loginData.userPassword='andyboss';
+loginData.userPassword='andyboss';*/
 
 describe('Browse to sign in', function() {
 
