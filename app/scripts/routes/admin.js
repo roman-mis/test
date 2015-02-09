f94eb24865764a3c5f11fc020282254e53724787
Ahmed Hashem <ahashem302@gmail.com>
2015-02-09 16:35:46 +0200

    added company profile pages and navigations

--- app/scripts/routes/admin.js
+++ app/scripts/routes/admin.js
@@ -16,7 +16,22 @@ angular.module("origApp").config(function($stateProvider) {
 	})
 	.state('app.admin.company_profile', {
 		url: '/company_profile',
-		templateUrl: 'views/admin/company_profile.html'
+		templateUrl: 'views/admin/company_profile/company_profile.html'
+	})
+	.state('app.admin.company_profile.contact', {
+		url: '/contact',
+		templateUrl: 'views/admin/company_profile/partials/contact.html'
+	}).state('app.admin.company_profile.accounts', {
+		url: '/accounts',
+		templateUrl: 'views/admin/company_profile/partials/accounts.html'
+	})
+	.state('app.admin.company_profile.bank_details', {
+		url: '/bank_details',
+		templateUrl: 'views/admin/company_profile/partials/bank_details.html'
+	})
+	.state('app.admin.company_profile.defaults', {
+		url: '/defaults',
+		templateUrl: 'views/admin/company_profile/partials/defaults.html'
 	})
 	.state('app.admin.addNew', {
     url: '/add_new/:type',
