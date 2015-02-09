5ff2596ac9085b122d689e9526c744fdb8f529ba
Sajan Maharjan <sazzzan.maharjan@gmail.com>
2015-02-09 19:14:35 +0545

    Invoice Design Tests

--- server/services/invoicedesignservice.js
+++ server/services/invoicedesignservice.js
@@ -1,39 +1,39 @@
 'use strict';
 
-var db = require('../models'),
-	Q=require('q'),
-	queryutils=require('../utils/queryutils')(db);
+module.exports=function(){
+	var db = require('../models'); 
+	var Q=require('q');
+	var queryutils=require('../utils/queryutils')(db);
 
-var service={};
+	var service={};
 
-service.getAllInvoiceDesigns = function(request){
-	return Q.Promise(function(resolve,reject){
-		var q=db.InvoiceDesign.find();
-		
-		queryutils.applySearch(q,db.InvoiceDesign,request)
-		.then(resolve,reject);
-	});
+	service.getAllInvoiceDesigns = function(request){
+		return Q.Promise(function(resolve,reject){
+			var q=db.InvoiceDesign.find();
+			queryutils.applySearch(q,db.InvoiceDesign,request)
+			.then(resolve,reject);
+		});
+	};
 
+	service.saveInvoiceDesign = function(invoiceId, invoiceDesign){
+		var deff = Q.defer();
+		var invoiceDesignModel;
+		invoiceDesignModel = new db.InvoiceDesign(invoiceDesign);
+		invoiceDesignModel.save(function(err){
+			if(err){
+				deff.reject(err);
+			}else{
+				console.log('save success');
+				deff.resolve(invoiceDesignModel);
+			}
+		});
+		return deff.promise;
+	};
+
+	service.getInvoiceDesign=function(id){
+		var q=db.InvoiceDesign.findById(id);
+		return Q.nfcall(q.exec.bind(q));
+	};
+
+	return service;
 };
-
-service.saveInvoiceDesign = function(invoiceId, invoiceDesign){
-	var deff = Q.defer();
-	var invoiceDesignModel;
-	invoiceDesignModel = new db.InvoiceDesign(invoiceDesign);
-	invoiceDesignModel.save(function(err){
-		if(err){
-			deff.reject(err);
-		}else{
-			console.log('save success');
-			deff.resolve(invoiceDesignModel);
-		}
-	});
-	return deff.promise;
-};
-
-service.getInvoiceDesign=function(id){
-	var q=db.InvoiceDesign.findById(id);
-	return Q.nfcall(q.exec.bind(q));
-};
-
-module.exports = service;
