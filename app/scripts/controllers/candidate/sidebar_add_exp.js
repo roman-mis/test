5d071e6a6a4a86798014cb0cba1b884f99df7450
Administrator <wangkang89@hotmail.com>
2015-02-10 18:32:18 +0800

    #7 add expenses wizard

--- app/scripts/controllers/candidate/sidebar_add_exp.js
+++ app/scripts/controllers/candidate/sidebar_add_exp.js
@@ -64,6 +64,142 @@ angular.module('origApp.controllers')
               }
             });
           };
+          
+          $scope.generateSummaries = function() {
+            var summaries = [];
+            $scope.expenseData.daysInRange.forEach(function(dateItem) {
+              if (dateItem.object === 'all') {
+                return;
+              }
+              var summary = {date: dateItem.object};
+
+              //get transports
+              summary.transport = 0;
+              $scope.expenseData.transports.forEach(function(item) {
+                if (item.date.getTime() === summary.date.getTime()) {
+                  summary.transport += item.cost * 1;
+                }
+              });
+
+              //get subsistence
+              summary.subsistence = 0;
+              $scope.expenseData.subsistences.forEach(function(item) {
+                if (item.date.getTime() === summary.date.getTime()) {
+                  summary.subsistence += item.cost * 1;
+                }
+              });
+
+              //get other
+              summary.other = 0;
+              $scope.expenseData.others.forEach(function(item) {
+                if (item.date.getTime() === summary.date.getTime()) {
+                  summary.other += item.cost * 1;
+                }
+              });
+
+              summary.total = summary.transport + summary.subsistence + summary.other;
+
+              summaries.push(summary);
+            });
+            
+            $scope.expenseData.summaries = summaries;
+            
+            return summaries;
+          };
+          
+          $scope.generateSendData = function(){
+            var data = [];
+            $scope.expenseData.daysInRange.forEach(function(dateItem) {
+              if (dateItem.object === 'all') {
+                return;
+              }
+              var dataItem = {date: dateItem.object, agency: $scope.expenseData.agency._id, expenses: []};
+              $scope.expenseData.times.forEach(function(item) {
+                if (item.date.getTime() === dataItem.date.getTime()) {
+                  dataItem.startTime = item.startTime + ':00';
+                  dataItem.endTime = item.endTime + ':00';
+                }
+              });
+              $scope.expenseData.postCodes.forEach(function(item) {
+                if (item.date.getTime() === dataItem.date.getTime()) {
+                  dataItem.postcodes = item.codes;
+                }
+              });
+
+              //get transports
+              $scope.expenseData.transports.forEach(function(item) {
+                if (item.date.getTime() === dataItem.date.getTime()) {
+                  dataItem.expenses.push({
+                    expenseType: 'Transport',
+                    subType: item.type.description,
+                    value: item.cost * 1
+                  });
+                }
+              });
+
+              //get subsistence
+              $scope.expenseData.subsistences.forEach(function(item) {
+                if (item.date.getTime() === dataItem.date.getTime()) {
+                  dataItem.expenses.push({
+                    expenseType: 'Subsistence',
+                    subType: item.type.description,
+                    value: item.cost * 1,
+                    description: item.description
+                  });
+                }
+              });
+
+              //get other
+              $scope.expenseData.others.forEach(function(item) {
+                if (item.date.getTime() === dataItem.date.getTime()) {
+                  dataItem.expenses.push({
+                    expenseType: 'Other',
+                    subType: item.type.description,
+                    value: item.cost * 1,
+                    description: item.description
+                  });
+                }
+              });
+
+              data.push(dataItem);
+            });
+            
+            $scope.expenseData.sendData = data;
+            
+            return data;
+          };
+          
+          $scope.getListDataFromSendData = function(sendData){
+            var listData = [];
+            sendData.forEach(function(dayItem){
+              dayItem.expenses.forEach(function(expenseItem){
+                var listItem = angular.copy(expenseItem);
+                listItem.date = dayItem.date;
+                listData.push(listItem);
+              });
+            });
+            return listData;
+          };
+          
+          $scope.attachReceiptsToSendData = function(receiptData){
+            if(!$scope.expenseData.sendData){
+              $scope.generateSendData();
+            }
+            var sendData = $scope.expenseData.sendData;
+            sendData.forEach(function(dataItem){
+              dataItem.expenses = [];
+              receiptData.forEach(function(item) {
+                if (item.date.getTime() === dataItem.date.getTime()) {
+                  var newItem = angular.copy(item);
+                  delete newItem.date;
+                  delete newItem.checked;
+                  dataItem.expenses.push(newItem);
+                }
+              });
+            });
+            $scope.expenseData.sendData = sendData;
+            return sendData;
+          };
 
           $(window).on('resize', $scope.normalizeTables);
         });
