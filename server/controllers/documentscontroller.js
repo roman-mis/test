3c7e269c2e014ced77959ef6e07d7b0ef953bdac
Ishwor Mali(MIS) <ishwor@makeitsimple.info>
2015-02-10 22:00:43 +0545

    refactored documents routes to supply folder

--- server/controllers/documentscontroller.js
+++ server/controllers/documentscontroller.js
@@ -1,19 +1,27 @@
 'use strict';
 
+function getFolderPath(req){
+    var folder=process.env.S3_TEMP_FOLDER;
+    if(req.params.folder && req.params.folder.toLowerCase()==='receipt'){
+        folder=process.env.S3_RECEIPT_FOLDER;
+        // console.log('process.env.S3_RECEIPT_FOLDER    '+process.env.S3_RECEIPT_FOLDER);
+    }
+
+    return folder;
+}
+
 module.exports = function(){
   
 
     var controller={};
     var awsService=require('../services/awsservice');
 
-
-
     controller.getUploadDocumentSignedUrl=function(req,res){
        var objectName=req.query.fileName;
             var objectType=req.query.mimeType;
             // var documentUpload=req.query.documentUpload||false;
-            var folder=process.env.S3_TEMP_FOLDER;
-
+            var folder=getFolderPath(req);
+            
         awsService.getS3SignedUrl('putObject', objectName,objectType,folder)
         .then(function(returnData){
             res.json(returnData);
@@ -25,7 +33,7 @@ module.exports = function(){
        var objectName=req.query.fileName;
          //   var objectType=req.query.mimeType;
             // var documentUpload=req.query.documentUpload||false;
-            var folder=process.env.S3_TEMP_FOLDER;
+            var folder=getFolderPath(req);
 
         awsService.getS3SignedUrl('getObject', objectName,null,folder)
         .then(function(returnData){
@@ -38,7 +46,7 @@ module.exports = function(){
        var objectName = req.params.fileName;
            // var objectType=req.query.mimeType;
             
-            var folder=process.env.S3_TEMP_FOLDER;
+            var folder=getFolderPath(req);
 
         awsService.deleteS3Object(objectName,folder)
         .then(function(){
--- server/routes/documentroutes.js
+++ server/routes/documentroutes.js
@@ -11,6 +11,7 @@ module.exports = function(app){
   app.use('/api/documents', expressJwt({secret:process.env.JWT_SECRET}), router);
 };
 
-router.get('/signTempS3',controller.getUploadDocumentSignedUrl);
-router.get('/signTempViewdocS3',controller.getViewDocumentSignedUrl);
-router.delete('/delete/:fileName',controller.deleteTempDocument);
+router.get('/signTempS3/:folder?',controller.getUploadDocumentSignedUrl);
+
+router.get('/signTempViewdocS3/:folder?',controller.getViewDocumentSignedUrl);
+router.delete('/delete/:fileName/:folder?',controller.deleteTempDocument);
