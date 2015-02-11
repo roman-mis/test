1b3ef9333bdd79a6ae272135dd3bbe8c0a0db074
Ahmed Hashem <ahashem302@gmail.com>
2015-02-11 20:26:19 +0200

    no message

--- app/scripts/controllers/admin/contactController.js
+++ app/scripts/controllers/admin/contactController.js
@@ -2,9 +2,37 @@ var app = angular.module('origApp.controllers');
 
 app.controller('contactController',['$scope', '$location', 'HttpResource', '$rootScope',
 	function($scope, $location, HttpResource, $rootScope){
+
     $rootScope.breadcrumbs = [{link:'/', text:'Home'},
                               {link: '/admin/home', text: 'Admin'},
                               {link: '/admin/company_profile/contact', text: 'Company Profile'}
-                              // {link: '/admin/home/company_profile/contact', text: 'Contact'}
                               ];
+
+    // var Session = $resource('api/adminCompanyProfileContact/:id', {}, {
+				// 	   	update: {
+				// 	      method: 'PUT'
+				// 	    }
+				// 	});
+
+    $scope.contact = {
+    					companyName: null,
+    					address1: null,
+    					address2: null,
+    					town: null,
+    					country: null,
+    					postCode: null,
+    					tel: null,
+    					fax: null,
+    					email: null
+    				};
+
+    var acAPI = HttpResource.model('admin/companyProfile/contact');
+
+    $scope.save = function(){
+    	console.log($scope.contact);
+    	acAPI.query({}, function(data) {
+    		console.log(data);
+    	});
+    }
+
 }]);
