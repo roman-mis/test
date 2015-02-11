3f8abd75f21055d7de1ed6dacc0250497c7b2b18
Andy Jones <andy@makeitsimple.info>
2015-02-11 15:04:41 +0000

    Fixed model name and autoIncrement

--- server/models/timesheetBatch.js
+++ server/models/timesheetBatch.js
@@ -3,10 +3,12 @@
 var Schema=require('mongoose').Schema;
 var BaseSchema=require(__dirname+'/baseschema');
 
-module.exports = function(mongoose) {
+module.exports = function(mongoose,autoIncrement) {
     var schema = new BaseSchema({
-        
+        batchNumber: Number    
     });
+    
+    schema.plugin(autoIncrement.plugin,{model:'TimesheetBatch',field:'batchNumber',startAt:1,incrementBy:1});
 
-    return mongoose.model('Timesheet',schema);
+    return mongoose.model('TimesheetBatch',schema);
 };
