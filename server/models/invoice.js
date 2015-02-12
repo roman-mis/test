fe085dbb6e392f0cfed5b3f9cb3bed35276eed22
Andy Jones <andy@makeitsimple.info>
2015-02-12 15:00:21 +0000

    New invoice schema

--- server/models/invoice.js
+++ server/models/invoice.js
@@ -3,31 +3,37 @@
 var Schema = require('mongoose').Schema;
 var BaseSchema=require(__dirname+'/baseschema');
 
-module.exports = function(mongoose) {
+module.exports = function(mongoose,autoIncrement) {
   	
 	var schema= new BaseSchema({
 		agency: { type: Schema.Types.ObjectId, ref:'Agency' },
 		branch: { type: Schema.Types.ObjectId, ref:'Branch' },
-		invoices: [Number],
-		conpanyDefaults:{
-			holidayPayIncluded: Boolean,
-			employeeNiIncluded: Boolean,
-			vatCharged: Boolean,
-			invoiceDesign: { type: Schema.Types.ObjectId, ref:'InvoiceDesign' },
-			marginAmount: Number,
-			invoiceEmailPrimary: String,
-			invoiceEmailSecondary: String,
-			paymentTerms: String,
-			marginChargedToAgency: String,
-			holidayAmount: String
-		},
-		totalNumberOfContractors: Number,
-		erniValueToAgency: Number,
-		holidayValueToAgency: Number,
-		totalInvoiceValue: Number,
-		totalVatValue: Number,
-		marginValueToAgency: Number
+        timesheetBatch: { type: Schema.Types.ObjectId, ref:'TimesheetBatch' },
+		date: { type: Date, default: Date.now },
+        invoiceNumber: Number,
+        dueDate: Date,
+        lines: [
+            {
+                worker: { type: Schema.Types.ObjectId, ref:'User' },
+                type: String, // timesheet, margin
+                elements: [
+                    {
+                        text: String,
+                        weekEndingDate: Date,
+                        units: Number,
+                        rate: Number,
+                        total: Number
+                    }
+                ]
+            }
+        ],
+        net: Number,
+        vatRate: Number,
+        vat: Number,
+        total: Number
 	});
+    
+    schema.plugin(autoIncrement.plugin,{model:'Invoice',field:'invoiceNumber',startAt:1,incrementBy:1});
     
     return mongoose.model('Invoice',schema);
 };
