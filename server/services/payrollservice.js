2710dabb1e15e602569b2c7566bcdaaa4c64ac28
Andy Jones <andy@makeitsimple.info>
2015-02-17 22:55:12 +0000

    Payroll day2 - Holiday Pay

--- server/services/payrollservice.js
+++ server/services/payrollservice.js
@@ -87,15 +87,6 @@ module.exports=function(){
                            });
                        }
 
-                       var _payrollProduct;
-                       
-                       // Get the payroll product
-                       _worker.payrollProduct.forEach(function(payrollProduct) {
-                           if(payrollProduct.agency===req.body.agency) {
-                               _payrollProduct=payrollProduct;
-                           }
-                       });
-
                        // Get all the timesheets for this worker that are status of receipted
                        db.Timesheet.find( { $and: [ { status: 'receipted' } , { worker: worker._id } ] })
                        .exec(function(timesheets,err) {
@@ -103,12 +94,24 @@ module.exports=function(){
                                if(timesheets.count>0) {
 
                                    var totalHours = 0,
-                                       totalPay = 0;
+                                       totalPay = 0,
+                                       totalEffectiveEarnings = 0,
+                                       totalHolidayPay = 0;
 
+                                   var timesheetIndex = 0;
                                    timesheets.forEach(function(timesheet) {
+                                       
+                                       var _payrollProduct;
+                                       // Get the payroll product
+                                       _worker.payrollProduct.forEach(function(payrollProduct) {
+                                           if(payrollProduct.agency===timesheet.agency) {
+                                               _payrollProduct=payrollProduct;
+                                           }
+                                       });
 
                                        log.push('Found timesheet: ' + timesheet);
 
+                                       var timesheetElementIndex = 0;
                                        timesheet.elements.forEach(function(element) {
 
                                            log.push('Found timesheet element: ' + element);
@@ -125,31 +128,80 @@ module.exports=function(){
                                                    hours = element.units;
                                                }
 
-                                               log.push('Adding ' + (element.units*element.payRate) + ' of pay from this element');
-                                               totalPay+=element.units*element.payRate;
+                                               // Work out the effective earnings based on hours * NMW
+                                               var effectiveEarnings = hours*nmw;
+                                               timesheet.elements[timesheetElementIndex].effectiveEarnings = effectiveEarnings;
+                                               totalEffectiveEarnings+=effectiveEarnings;
+                                               log.push('Effective earnings: ' + effectiveEarnings);
+                                               
+                                               var pay = element.units*element.payRate;
+                                               log.push('Adding ' + pay + ' of pay from this element');
+                                               totalPay+=pay;
 
                                                log.push('Adding ' + hours + ' hours from this element');
                                                totalHours+=hours;
 
                                                log.push('Now the total hours for this worker are: ' + totalHours);
+                                               
+                                               //region Holiday pay
+                                               
+                                               // Rolled up or retained but on Total Pay
+                                               var holidayPay = 0;
+                                               if(_payrollProduct.holidayPayRule==='2' || _payrollProduct.holidayPayRule==='4') {
+                                                   holidayPay = (_payrollProduct.holidayPayDays/260)*pay;
+                                                   timesheet.elements[timesheetElementIndex].holidayPay.onActual = holidayPay;
+                                                   totalHolidayPay+=holidayPay;
+                                               }
+                                               // Rolled up or retained on NMW
+                                               if(_payrollProduct.holidayPayRule==='1' || _payrollProduct.holidayPayRule==='3') {
+                                                   holidayPay = (_payrollProduct.holidayPayDays/(260-_payrollProduct.holidayPayDays))*effectiveEarnings;
+                                                   timesheet.elements[timesheetElementIndex].holidayPay.onNMW = holidayPay;
+                                                   totalHolidayPay+=holidayPay;
+                                               }
+                                               
+                                               
+                                               //endregion
                                            }
+                                           
+                                           if(element.paymentRate.rateType==='Holiday') {
+                                               var holidayPay = units*payRate;
+                                               totalHolidayPay+=holidayPay;
+                                               
+                                               totalPay+=holidayPay;
+                                            
+                                               // Rolled up or retained but on Total Pay
+                                               if(_payrollProduct.holidayPayRule==='2' || _payrollProduct.holidayPayRule==='4') {
+                                                   timesheet.elements[timesheetElementIndex].holidayPay.onActual = holidayPay;
+                                               }
+                                               // Rolled up or retained on NMW
+                                               if(_payrollProduct.holidayPayRule==='1' || _payrollProduct.holidayPayRule==='3') {
+                                                   timesheet.elements[timesheetElementIndex].holidayPay.onNMW = holidayPay;
+                                               }
+                                           }
+                                           
+                                           timesheetElementIndex++;
                                        });
+                                       
+                                       timesheet[timesheetIndex].save(function(err,user){
+                                           log.push('Error saving timesheet: ' + err);
+                                       });
+                                       
+                                       timesheetIndex++;
                                    });
 
-                                   // Work out the effective earnings based on hours * NMW
-                                   var effectiveEarnings = totalHours*nmw;
-                                   log.push('Effective earnings: ' + effectiveEarnings);
-
+                                   //region Earnings
+                                   
                                    var employersNiOnNmw = 0;
 
-                                   if(effectiveEarnings>employersNiThreshold) {
+                                   if(effectiveEarnings>=employersNiThreshold) {
                                        employersNiOnNmw = ((effectiveEarnings-employersNiThreshold)/100)*employersNiRate;
                                        log.push('Employers NI on NMW: ' + employersNiOnNmw);
                                    }
 
                                    var actualNMW = effectiveEarnings+employersNiOnNmw;
                                    log.push('Actual NMW: ' + actualNMW);
-
+                                   
+                                   //endregion
                                    
                                }
                                else {
