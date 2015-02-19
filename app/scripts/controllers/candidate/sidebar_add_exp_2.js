7c8d9c29de9c91b79fb52b2dfb58d7a31c3846bb
ndxbxrme <lewis_the_cat@hotmail.com>
2015-02-19 20:56:14 +0000

    expenses agencies from payrollproduct

--- app/scripts/controllers/candidate/sidebar_add_exp_2.js
+++ app/scripts/controllers/candidate/sidebar_add_exp_2.js
@@ -1,6 +1,16 @@
 'use strict';
 angular.module('origApp.controllers')
         .controller('CandidateSidebarAddExp2Controller', function($scope, HttpResource) {
-          $scope.agencies = HttpResource.model('agencies').query({});
+          console.log($scope.expenseData, $scope.mainData.candidateId);
+          var payrollProducts = HttpResource.model('candidates/' + $scope.mainData.candidateId + '/payrollproduct').query({}, function(){
+            $scope.agencies = [];
+            angular.forEach(payrollProducts, function(pp) {
+              if($scope.agencies.indexOf(pp.agency)===-1) {
+                $scope.agencies.push(pp.agency); 
+              }
+            });
+          });
+          //$scope.agencies = HttpResource.model('agencies').query({});
         });
 
+  
