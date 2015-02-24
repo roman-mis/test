71146f759039490e0015b55d48dbdfe87a296b9a
boojaka <boojaka@gmail.com>
2015-02-24 12:48:25 +0200

    activity dialog + broken document dailog

--- test/e2e/spec/sidebar.js
+++ test/e2e/spec/sidebar.js
@@ -2,7 +2,7 @@ var helper = require('./ui-helper.js');
 
 
 
-/* to remove ================================>
+/* to remove ================================>*/
 
 describe('Navigate to candidates url', function () {
 
@@ -33,7 +33,7 @@ describe('Navigate to candidates url', function () {
 
   });
 });
- */
+
 
 var clickFirstVisible=function(locator,callback){
   element.all(locator).filter(function(elem, index) {
@@ -53,6 +53,88 @@ var testModal=function(locator){
     link.click();
   })
 };
+
+
+
+
+describe('Checking Activity', function() {
+
+  it('should open Call-log dialog', function () {
+    clickFirstVisible(by.css('[ng-click="openCreateDocumentWin({})"]'), function (link) {
+      link.click();
+      expect($('.modal-content').isDisplayed()).toBeTruthy();
+    });
+  });
+
+  it('should open Call-log dialog', function () {
+    helper.selectSelector(element(by.model('data.agency')),1);
+    helper.selectSelector(element(by.model('data.documentType')),1);
+    helper.selectSelector(element(by.model('data.documentType')),1);
+    element(by.model('data.documentName')).clear().sendKeys('asd');
+
+    var path = require('path');
+    var fileToUpload = '../1.png';
+    var absolutePath = path.resolve(__dirname, fileToUpload);
+    console.log(absolutePath);
+    $('[ng-model="data.file"]').sendKeys(absolutePath);
+    $('[ng-click="uploadFile()"]').click();
+
+    element.all(by.repeater('file in files')).count().then(function(n){
+      console.log('count='+n);
+      expect(n).toBeGreaterThan(0);
+    });
+
+  });
+
+});
+
+/*
+describe('Checking Activity', function() {
+   var openActivityDialog=function(){
+   clickFirstVisible(by.css('[ng-click="openAddActivityWin()"]'),function(link){
+   link.click();
+   expect($('.modal-content').isDisplayed()).toBeTruthy();
+   });
+   };
+
+  it('should open Call-log dialog', function () {
+    openActivityDialog();
+    helper.selectSelector(element(by.model('data.agency')),1);
+    helper.selectSelector(element(by.model('data.activityType')),0);
+    $('.modal-content [ng-click="next()"]').click();
+
+    expect($('.modal-title [ng-show="activityType==\'callLog\'"]').getText()).toContain('call log');
+    expect($('.modal-title [ng-show="activityType==\'callLog\'"]').isDisplayed()).toBeTruthy();
+    $('.modal-content [ng-click="cancel()"]').click();
+    expect($('.modal-content').isPresent()).toBeFalsy();
+  });
+
+  it('should open Task-Wizard dialog ', function () {
+    openActivityDialog();
+    helper.selectSelector(element(by.model('data.agency')),1);
+    helper.selectSelector(element(by.model('data.activityType')),1);
+    $('.modal-content [ng-click="next()"]').click();
+
+    expect($('.modal-title [ng-show="activityType==\'task\'"]').getText()).toContain('task wizard');
+    expect($('.modal-title [ng-show="activityType==\'task\'"]').isDisplayed()).toBeTruthy();
+    $('.modal-content [ng-click="cancel()"]').click();
+    expect($('.modal-content').isPresent()).toBeFalsy();
+  });
+
+  it('should open Document-wizard dialog ', function () {
+    openActivityDialog();
+    helper.selectSelector(element(by.model('data.agency')),1);
+    helper.selectSelector(element(by.model('data.activityType')),2);
+    $('.modal-content [ng-click="next()"]').click();
+
+    expect($('.modal-title').getText()).toContain('documents wizard');
+    expect($('.modal-title').isDisplayed()).toBeTruthy();
+    $('.modal-content [ng-click="cancel()"]').click();
+    expect($('.modal-content').isPresent()).toBeFalsy();
+  });
+
+});
+
 
 
 
@@ -152,8 +234,10 @@ describe('checking expense wizard', function() {
 
   it('selecting agency and default date', function () {
     element(by.css('[ng-click="gotoNext()"]')).click();
-    /*element(by.css('.modal-content .select2-choice')).click();
-    element.all(by.css('#select2-results-2 li')).get(0).click();*/
+    */
+/*element(by.css('.modal-content .select2-choice')).click();
+    element.all(by.css('#select2-results-2 li')).get(0).click();*//*
+
     helper.selectSelector(element(by.model('expenseData.agency')),1);
     element(by.css('[ng-click="gotoNext()"]')).click();
     okBtn.click();
@@ -303,6 +387,7 @@ describe('checking expense wizard', function() {
 
 });
 
+*/
 
 /*  DONT WORK
 
