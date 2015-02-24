51c22bc5a9b247f5f67376f5210f000487fb251f
mostafa ahmed <mostafa.ahmed91@gmail.com>
2015-02-24 15:26:50 +0200

    payroll run

--- app/scripts/controllers/payroll/main.js
+++ app/scripts/controllers/payroll/main.js
@@ -79,8 +79,8 @@ app.controller('PayrollMainController',['$state', '$rootScope', '$scope', 'HttpR
     		}
     	}};
     	console.log(params);
-    	HttpResource.model('candidates').query(params,function(data){
-		// HttpResource.model('users').customGet('',{},function(data){
+    	// HttpResource.model('candidates').query(params,function(data){
+		HttpResource.model('users').customGet('',{},function(data){
 			console.log('done !! candidates');
 	      	console.log(data.data.objects);
 	      	$scope.candidates = data.data.objects;
@@ -101,6 +101,11 @@ app.controller('PayrollMainController',['$state', '$rootScope', '$scope', 'HttpR
     		$scope.runPayroll.worker[i] = $scope.selection.type;	
     	}
 	}
+
+	$scope.Runpayroll = function(){
+		
+	}
+
 
 	$scope.test = function(){
 		console.log($scope.pay.frequency);
