a5e483d232545ac7d1b071d9b90240aa2424f917
ndxbxrme <lewis_the_cat@hotmail.com>
2015-02-25 07:24:15 +0000

    admin server controllers, services & routes

---
+++ server/controllers/admin/adminMileageRates.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var adminMileageRatesService = require('../../services/admin/adminMileageRatesSerivce');
+
+module.exports = function() {
+    
+    return {
+        getMileageRates: function(req, res) {
+            
+        },
+        saveMileageRates: function(req, res) {
+            
+        }
+    };
+};
