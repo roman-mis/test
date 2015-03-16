2bf8b8ebb307cb33f245db657f43755d1364fecd
ed299 <mohammad.mahmoud91@gmail.com>
2015-03-16 20:54:56 +0200

    added yes/no field

--- app/scripts/controllers/candidate/payroll_tax.js
+++ app/scripts/controllers/candidate/payroll_tax.js
@@ -1,11 +1,20 @@
 'use strict';
 angular.module('origApp.controllers')
         .controller('CandidatePayrollTaxController', function($scope, $stateParams, HttpResource) {
+          $scope.employeesNIpaid = {};
           $scope.candidateId = $stateParams.candidateId;
 
           //define private variables
           var candidateResource = HttpResource.model('candidates/' + $scope.candidateId);
-          
+          HttpResource.model('candidates/' + $scope.candidateId + '/taxdetail').query({}, function (res) {
+            $scope.employeesNIpaid.value = res.data.object.employeesNIpaid;
+          });
+
+          $scope.employeesNIpaidOptions = [
+              {value:'Yes',code: true},
+              {value: 'No',code: false}
+            ];
+            
           //load tax information
           $scope.loadTax = function() {
             $scope.tax = candidateResource.get('payrolltax');
