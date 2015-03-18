b4c5114f6646c4d866fc54b3dc8c6b7b4dc2dd3d
MahmoudEzzatAbdul-Rahman <mahmoud.ezzat.a.r@gmail.com>
2015-03-18 20:51:49 +0200

    Agengy home tab validation

--- app/scripts/controllers/agency/home.js
+++ app/scripts/controllers/agency/home.js
@@ -1,129 +1,133 @@
 'use strict';
 
 angular.module('origApp.controllers')
-        .controller('AgencyHomeController', function($scope, $stateParams, ModalService, HttpResource, ConstantsResource, $http) {
-          $scope.agencyId = $stateParams.agencyId;
-          $scope.countries = ConstantsResource.get('countries');
+        .controller('AgencyHomeController', function ($scope, $stateParams, ModalService, HttpResource, ConstantsResource, $http) {
+            $scope.agencyId = $stateParams.agencyId;
+            $scope.countries = ConstantsResource.get('countries');
+            //console.log($scope.$parent.agencyStatus);
 
-          function loadAgency() {
-            $scope.agency = HttpResource.model('agencies').get($scope.agencyId);
-          }
-          
-          function loadAgencyContact() {
-            $scope.agencyContact = HttpResource.model('agencies/' + $scope.agencyId).get('contact');
-          }
-          
-          
-          $scope.getConstant = function(constantKey, code) {
-            var hashData = ConstantsResource.getHashData(constantKey);
-            if(!hashData || !hashData[code]){
-              return {};
+            function loadAgency() {
+                $scope.agency = HttpResource.model('agencies').get($scope.agencyId);
             }
-            return hashData[code];
-          };
-          
-          $scope.openAgencyEdit = function() {
-            ModalService.open({
-              templateUrl: 'views/agency/_modal_agency.html',
-              parentScope: $scope,
-              controller: 'AgencyEditController'
-            });
-          };
 
-          
-          
-          $scope.openContactEdit = function() {
-            ModalService.open({
-              templateUrl: 'views/agency/_agency_contact_edit.html',
-              parentScope: $scope,
-              controller: '_AgencyContactEditController'
-            });
-          };
-          
-          //upload company logo
-          $scope.onSelectCompanyLogo = function(fileInput) {
-            $scope.$apply(function() {
-              $scope.agencyContact.localLogoPath = fileInput.value;
-            });
-          };
-          
-          //upload file to s3
-          $scope.uploadComapnyLogo = function() {
-            if(!$('#upload_company_logo').val()){
-              alert('Please select a file first.');
-              return;
+            function loadAgencyContact() {
+                $scope.agencyContact = HttpResource.model('agencies/' + $scope.agencyId).get('contact');
             }
-            var file = $('#upload_company_logo')[0].files[0];
-            var fileName = new Date().getTime().toString() + '_' + file.name;
-            var mimeType = file.type || 'text/plain';
-            $scope.isLogoUploading = true;
-            HttpResource.model('agencies/' + $scope.agencyId).customGet('logosignedurl', {
-              mimeType: mimeType,
-              fileName: fileName
-            }, function(response) {
-              var signedRequest = response.data.signedRequest;
-              $http({
-                method: 'PUT',
-                url: signedRequest,
-                data: file,
-                headers: {'Content-Type': mimeType, 'x-amz-acl': 'public-read'}
-              }).success(function() {
-                HttpResource.model('agencies/' + $scope.agencyId).create({logo: fileName})
-                    .patch('contact')
-                    .then(function(response) {
-                      $scope.isLogoUploading = false;
-                      if (!HttpResource.flushError(response)) {
-                        $scope.agencyContact.localLogoPath = '';
-                        $scope.agencyContact.logo = fileName;
-                      }
+
+
+            $scope.getConstant = function (constantKey, code) {
+                var hashData = ConstantsResource.getHashData(constantKey);
+                if (!hashData || !hashData[code]) {
+                    return {};
+                }
+                return hashData[code];
+            };
+
+            $scope.openAgencyEdit = function () {
+                ModalService.open({
+                    templateUrl: 'views/agency/_modal_agency.html',
+                    parentScope: $scope,
+                    controller: 'AgencyEditController'
+                });
+            };
+
+
+
+            $scope.openContactEdit = function () {
+                ModalService.open({
+                    templateUrl: 'views/agency/_agency_contact_edit.html',
+                    parentScope: $scope,
+                    controller: '_AgencyContactEditController'
+                });
+            };
+
+            //upload company logo
+            $scope.onSelectCompanyLogo = function (fileInput) {
+                $scope.$apply(function () {
+                    $scope.agencyContact.localLogoPath = fileInput.value;
+                });
+            };
+
+            //upload file to s3
+            $scope.uploadComapnyLogo = function () {
+                if (!$('#upload_company_logo').val()) {
+                    alert('Please select a file first.');
+                    return;
+                }
+                var file = $('#upload_company_logo')[0].files[0];
+                var fileName = new Date().getTime().toString() + '_' + file.name;
+                var mimeType = file.type || 'text/plain';
+                $scope.isLogoUploading = true;
+                HttpResource.model('agencies/' + $scope.agencyId).customGet('logosignedurl', {
+                    mimeType: mimeType,
+                    fileName: fileName
+                }, function (response) {
+                    var signedRequest = response.data.signedRequest;
+                    $http({
+                        method: 'PUT',
+                        url: signedRequest,
+                        data: file,
+                        headers: { 'Content-Type': mimeType, 'x-amz-acl': 'public-read' }
+                    }).success(function () {
+                        HttpResource.model('agencies/' + $scope.agencyId).create({ logo: fileName })
+                            .patch('contact')
+                            .then(function (response) {
+                                $scope.isLogoUploading = false;
+                                if (!HttpResource.flushError(response)) {
+                                    $scope.agencyContact.localLogoPath = '';
+                                    $scope.agencyContact.logo = fileName;
+                                }
+                            });
                     });
-              });
-            });
-          };
-          
-          loadAgency();
-          loadAgencyContact();
-          
-          $scope.addSubBreadcrumb(null);
-          
+                });
+            };
+
+            loadAgency();
+            loadAgencyContact();
+
+            $scope.addSubBreadcrumb(null);
+
         })
 
         // edit agency contact information
-        .controller('_AgencyContactEditController', function($scope, $modalInstance, parentScope, HttpResource) {
-          $scope.fields = [
-            {name: 'phone1', label: 'Phone 1'},
-            {name: 'phone2', label: 'Phone 2'},
-            {name: 'fax', label: 'Fax'},
-            {name: 'facebook', label: 'Facebook'},
-            {name: 'linkedin', label: 'Linkedin'},
-            {name: 'website', label: 'Website'},
-            {name: 'email', label: 'Email', type: 'email'}
-          ];
-          $scope.data = {};
+        .controller('_AgencyContactEditController', function ($scope, $modalInstance, parentScope, HttpResource) {
+            $scope.status = parentScope.$parent.agencyStatus;
+            $scope.ukPhone = /^[0]+(\d{10})+$/;
+            $scope.emailPat = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
+            $scope.fields = [
+              { name: 'phone1', label: 'Phone 1', required: ($scope.status == 'Live') ? true : false },
+              { name: 'phone2', label: 'Phone 2' },
+              { name: 'fax', label: 'Fax' },
+              { name: 'facebook', label: 'Facebook' },
+              { name: 'linkedin', label: 'Linkedin' },
+              { name: 'website', label: 'Website', type: 'url' },
+              { name: 'email', label: 'Email', type: 'email', required: ($scope.status == 'Live') ? true : false, pat: $scope.emailPat }
+            ];
+            $scope.data = {};
 
-          if(parentScope.agencyContact){
-            $scope.fields.forEach(function(item) {
-              $scope.data[item.name] = parentScope.agencyContact[item.name];
-            });
-          }
+            if (parentScope.agencyContact) {
+                $scope.fields.forEach(function (item) {
+                    $scope.data[item.name] = parentScope.agencyContact[item.name];
+                });
+            }
 
-          $scope.cancel = function() {
-            $modalInstance.dismiss('cancel');
-          };
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
 
-          $scope.ok = function() {
-            $scope.isSaving = true;
-            HttpResource.model('agencies/' + parentScope.agencyId).create($scope.data)
-                    .patch('contact')
-                    .then(function(response) {
-                      $scope.isSaving = false;
-                      if (!HttpResource.flushError(response)) {
-                        parentScope.agencyContact = jQuery.extend(parentScope.agencyContact, $scope.data);
-                        $modalInstance.close();
-                      }
-                    });
+            $scope.ok = function () {
+                $scope.isSaving = true;
+                HttpResource.model('agencies/' + parentScope.agencyId).create($scope.data)
+                        .patch('contact')
+                        .then(function (response) {
+                            $scope.isSaving = false;
+                            if (!HttpResource.flushError(response)) {
+                                parentScope.agencyContact = jQuery.extend(parentScope.agencyContact, $scope.data);
+                                $modalInstance.close();
+                            }
+                        });
 
-          };
+            };
         });
-        
-        
+
+
--- app/scripts/controllers/agency/payroll.js
+++ app/scripts/controllers/agency/payroll.js
@@ -1,110 +1,111 @@
 'use strict';
 angular.module('origApp.controllers')
-        .controller('AgencyPayrollController', function($scope, ModalService, $stateParams, HttpResource, ConstantsResource) {
-          $scope.agencyId = $stateParams.agencyId;
-  
-          function loadPayrollData() {
-            $scope.payrollData = HttpResource.model('agencies/' + $scope.agencyId).get('payroll');
-          }
-    
-          $scope.getConstant = function(constantKey, code) {
-            var hashData = ConstantsResource.getHashData(constantKey);
-            if (!hashData || !hashData[code]) {
-              return {};
+        .controller('AgencyPayrollController', function ($scope, ModalService, $stateParams, HttpResource, ConstantsResource) {
+            $scope.agencyId = $stateParams.agencyId;
+
+            function loadPayrollData() {
+                $scope.payrollData = HttpResource.model('agencies/' + $scope.agencyId).get('payroll');
             }
-            return hashData[code];
-          };
 
-          $scope.openAgencyDefaultInvoicing = function() {
-            ModalService.open({
-              templateUrl: 'views/agency/_edit_default_invoicing.html',
-              parentScope: $scope,
-              controller: '_EditAgencyDefaultInvoicing',
-              size: 'lg'
-            });
-          };
+            $scope.getConstant = function (constantKey, code) {
+                var hashData = ConstantsResource.getHashData(constantKey);
+                if (!hashData || !hashData[code]) {
+                    return {};
+                }
+                return hashData[code];
+            };
 
-          $scope.openAgencyDefaultPayroll = function() {
-            ModalService.open({
-              templateUrl: 'views/agency/_edit_default_payroll.html',
-              parentScope: $scope,
-              controller: '_EditAgencyDefaultPayroll'
-            });
-          };
-          
-          loadPayrollData();
+            $scope.openAgencyDefaultInvoicing = function () {
+                ModalService.open({
+                    templateUrl: 'views/agency/_edit_default_invoicing.html',
+                    parentScope: $scope,
+                    controller: '_EditAgencyDefaultInvoicing',
+                    size: 'lg'
+                });
+            };
+
+            $scope.openAgencyDefaultPayroll = function () {
+                ModalService.open({
+                    templateUrl: 'views/agency/_edit_default_payroll.html',
+                    parentScope: $scope,
+                    controller: '_EditAgencyDefaultPayroll'
+                });
+            };
+
+            loadPayrollData();
 
         })
 
         //Edit Agency Default Invoicing
-        .controller('_EditAgencyDefaultInvoicing', function($scope, $modalInstance, parentScope, HttpResource, ConstantsResource) {
-          $scope.data = {};
-          angular.copy(parentScope.payrollData.defaultInvoicing, $scope.data);
-          for(var key in $scope.data){
-            if($scope.data[key]){
-              if($scope.data[key].code){
-                $scope.data[key] = $scope.data[key].code;
-              }
-              if($scope.data[key]._id){
-                $scope.data[key] = $scope.data[key]._id;
-              }
+        .controller('_EditAgencyDefaultInvoicing', function ($scope, $modalInstance, parentScope, HttpResource, ConstantsResource) {
+            $scope.data = {};
+            $scope.status = parentScope.$parent.agencyStatus;
+            angular.copy(parentScope.payrollData.defaultInvoicing, $scope.data);
+            for (var key in $scope.data) {
+                if ($scope.data[key]) {
+                    if ($scope.data[key].code) {
+                        $scope.data[key] = $scope.data[key].code;
+                    }
+                    if ($scope.data[key]._id) {
+                        $scope.data[key] = $scope.data[key]._id;
+                    }
+                }
             }
-          }
-          
-          $scope.invoiceMethods = ConstantsResource.get('invoicemethods');
-          $scope.paymentTerms = ConstantsResource.get('paymentterms');
-          $scope.invoiceDesigns = HttpResource.model('invoicedesigns').query({});
-          
-          $scope.agency = HttpResource.model('agencies').get(parentScope.agencyId);
-          
-          $scope.cancel = function() {
-            $modalInstance.dismiss('cancel');
-          };
-          $scope.ok = function() {
-            $scope.isSaving = true;
-            HttpResource.model('agencies/' + parentScope.agencyId).create($scope.data)
-                    .patch('payroll')
-                    .then(function(response) {
-                      $scope.isSaving = false;
-                      if (!HttpResource.flushError(response)) {
-                        parentScope.payrollData = jQuery.extend(parentScope.payrollData, response.data.object);
-                        $modalInstance.close();
-                      }
-                    });
-          };
+
+            $scope.invoiceMethods = ConstantsResource.get('invoicemethods');
+            $scope.paymentTerms = ConstantsResource.get('paymentterms');
+            $scope.invoiceDesigns = HttpResource.model('invoicedesigns').query({});
+
+            $scope.agency = HttpResource.model('agencies').get(parentScope.agencyId);
+
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
+            $scope.ok = function () {
+                $scope.isSaving = true;
+                HttpResource.model('agencies/' + parentScope.agencyId).create($scope.data)
+                        .patch('payroll')
+                        .then(function (response) {
+                            $scope.isSaving = false;
+                            if (!HttpResource.flushError(response)) {
+                                parentScope.payrollData = jQuery.extend(parentScope.payrollData, response.data.object);
+                                $modalInstance.close();
+                            }
+                        });
+            };
         })
 
         //Edit Agency Default Payroll
-        .controller('_EditAgencyDefaultPayroll', function($scope, $modalInstance, parentScope, HttpResource, ConstantsResource) {
-          $scope.data = {};
-          angular.copy(parentScope.payrollData.defaultPayroll, $scope.data);
-          for(var key in $scope.data){
-            if($scope.data[key]){
-              if($scope.data[key].code){
-                $scope.data[key] = $scope.data[key].code;
-              }
-              if($scope.data[key]._id){
-                $scope.data[key] = $scope.data[key]._id;
-              }
+        .controller('_EditAgencyDefaultPayroll', function ($scope, $modalInstance, parentScope, HttpResource, ConstantsResource) {
+            $scope.data = {};
+            angular.copy(parentScope.payrollData.defaultPayroll, $scope.data);
+            for (var key in $scope.data) {
+                if ($scope.data[key]) {
+                    if ($scope.data[key].code) {
+                        $scope.data[key] = $scope.data[key].code;
+                    }
+                    if ($scope.data[key]._id) {
+                        $scope.data[key] = $scope.data[key]._id;
+                    }
+                }
             }
-          }
-  
-          $scope.serviceUsed = ConstantsResource.get('servicesused');
-          $scope.marginTypes = ConstantsResource.get('margintypes');
-          
-          $scope.cancel = function() {
-            $modalInstance.dismiss('cancel');
-          };
-          $scope.ok = function() {
-            $scope.isSaving = true;
-            HttpResource.model('agencies/' + parentScope.agencyId).create($scope.data)
-                    .patch('payroll')
-                    .then(function(response) {
-                      $scope.isSaving = false;
-                      if (!HttpResource.flushError(response)) {
-                        parentScope.payrollData = jQuery.extend(parentScope.payrollData, response.data.object);
-                        $modalInstance.close();
-                      }
-                    });
-          };
+
+            $scope.serviceUsed = ConstantsResource.get('servicesused');
+            $scope.marginTypes = ConstantsResource.get('margintypes');
+
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
+            $scope.ok = function () {
+                $scope.isSaving = true;
+                HttpResource.model('agencies/' + parentScope.agencyId).create($scope.data)
+                        .patch('payroll')
+                        .then(function (response) {
+                            $scope.isSaving = false;
+                            if (!HttpResource.flushError(response)) {
+                                parentScope.payrollData = jQuery.extend(parentScope.payrollData, response.data.object);
+                                $modalInstance.close();
+                            }
+                        });
+            };
         });
