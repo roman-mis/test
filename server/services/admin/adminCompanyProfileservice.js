e0811a299568151b1a746632833f8ccdb457e4c7
ishwor-mis <ishwor@makeitsimple.info>
2015-03-18 12:14:35 +0545

    refactored save company profile

--- server/services/admin/adminCompanyProfileservice.js
+++ server/services/admin/adminCompanyProfileservice.js
@@ -3,46 +3,36 @@ module.exports=function(db){
 
 var db 			= require('../../models');
 var Q 			= require('q');
-var queryutils 	= require('../../utils/queryutils')(db);
+var utils=require('../../utils/utils');
+// var queryutils 	= require('../../utils/queryutils')(db);
 var systemservice = require('../../services/systemservice')(db);
 var service = {};
 
 service.updateAdminCompanyProfile = function(id,name,val){
 console.log(val);
     var q=Q.defer();
-    if(name==='profile'){
-    	db.System.update({'_id':id},{$set:{'companyProfile.contact':val}},function(err){
-			if(err){
-			q.reject(err);
-			}else{
-				q.resolve({});
+    systemservice.getSystem()
+    	.then(function(system){
+    		
+    		////console.log(system);
+    		if(name.toLowerCase()==='profile'){
+    			utils.updateSubModel(system.contact,val);
+
+		    }else if(name.toLowerCase() === 'accounts'){
+				utils.updateSubModel(system.accounts,val);		    	
+		    }else if(name.toLowerCase() === 'bankdetails'){
+				utils.updateSubModel(system.bankDetails,val);		    	
+		    	
+		    }else if(name.toLowerCase() === 'defaults'){
+		    	utils.updateSubModel(system.defaults,val);		    	
 			}
-		}) 
-    }else if(name === 'accounts'){
-    	db.System.update({'_id':id},{$set:{'companyProfile.accounts':val}},function(err){
-			if(err){
-			q.reject(err);
-			}else{
-				q.resolve({});
-			}
-		}) 
-    }else if(name === 'bankDetails'){
-    	db.System.update({'_id':id},{$set:{'companyProfile.bankDetails':val}},function(err){
-			if(err){
-			q.reject(err);
-			}else{
-				q.resolve({});
-			}
-		}) 
-    }else if(name === 'defaults'){
-    	db.System.update({'_id':id},{$set:{'companyProfile.defaults':val}},function(err){
-			if(err){
-			q.reject(err);
-			}else{
-				q.resolve({});
-			}
-		}) 
-    }
+			//console.log('system.save');
+				console.log('------system-------');
+			 return Q.nfcall(system.save.bind(system));
+			
+    	})
+    	 .then(q.resolve,q.reject);
+
   
 	return q.promise;
 };
