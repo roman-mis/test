374f08b599c8faba64c2a663c2c6ffe5270586da
Mohamed Mahmoud <mohamed@makeitsimple.info>
2015-03-19 18:37:11 +0200

    unfinished worker validation

--- app/scripts/controllers/candidate/editCandidateDetails.js
+++ app/scripts/controllers/candidate/editCandidateDetails.js
@@ -1,13 +1,74 @@
 'use strict';
 angular.module('origApp.controllers')
-		.controller('editContactDetailsCtrl', ['$scope', '$modalInstance', '$stateParams', 'HttpResource','parentScope',
+		.controller('editContactDetailsCtrl', ['$scope', '$modalInstance', '$stateParams', 'HttpResource','parentScope','ConstantsResource',
 			function($scope,$modalInstance,$stateParams, HttpResource, parentScope){
 				$scope.candidateId = $stateParams.candidateId;
-				console.log($scope.candidateId);
-
-				HttpResource.model('constants/candidateStatus').query({},function (res) {
+				$scope.candidate = parentScope.candidate;
+				
+				
+				console.log($scope.candidate);
+				HttpResource.model('candidates/' + $scope.candidateId+'/contactdetail').query({},function (res) {
 					console.log(res);
 				});
-				$scope.parent = parentScope;
-				console.log($scope.parent);
+				
+				$scope.status = parentScope.candidate.status;
+				$scope.lettersOnly = /^[A-Za-z]+$/;
+				$scope.ukMobile = /^0(\d ?){10}$/;
+				console.log(parentScope.candidate.status);
+
+
+				HttpResource.model('constants/nationalities').query({},function (res) {
+					// body...
+					$scope.nationalities = res.data;
+				});
+				console.log($scope.nationalities);
+				
+
+
+
+				$scope.calcAge = function (dateString) {
+					var birthday = +new Date(dateString);
+					return ~~((Date.now() - birthday) / (31557600000));
+				};
+
+
+				$scope.$watch('form.age',function () {
+					if($scope.form.age){
+
+						$scope.$watch('candidate.birthDate', function (newVal) {
+							$scope.candidate.birthDate = newVal;
+							console.log($scope.candidate.birthDate);
+							$scope.age=$scope.calcAge($scope.candidate.birthDate);
+							console.log($scope.age);
+
+							
+								// $scope.form.age = newVal;
+								if($scope.age<16 && $scope.status ==='Live'){
+									$scope.isValid = false;
+									console.log($scope.isValid);
+									$scope.form.age.$setValidity('age',$scope.isValid);
+								}else{
+									$scope.isValid = true;
+									console.log($scope.isValid);
+									$scope.form.age.$setValidity('age',$scope.isValid);
+								}
+							
+
+						});
+					}
+				});
+				
+				
+				//console.log($scope.age);
+				$scope.$watch('candidate.birthDate', function () {
+					
+				});
+				
+				//
+				$scope.log = function () {
+					// boy...
+					console.log($scope.form);
+					console.log($scope.form.age.$error);
+					console.log($scope.nationalities);
+				};
 		}]);
