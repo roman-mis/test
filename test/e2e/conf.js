241c709d7f120bf75735f10ffd50a8d517e25a03
boojaka <boojaka@gmail.com>
2015-03-19 23:05:21 +0200

    excluding cadidates check

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -18,9 +18,9 @@ exports.config = {
     'browserName': 'chrome'
   }],*/
   suites: {
-    main:['./spec/reg.js','./spec/check-inbox.js','./spec/activate.js','./spec/login.js','./spec/agency_prefill.js','./spec/search_current_candidate.js','./spec/candidates.js','./spec/sidebar.js'],
+    main:['./spec/reg.js','./spec/check-inbox.js','./spec/activate.js','./spec/login.js','./spec/agency_prefill.js','./spec/search_current_candidate.js'/*,'./spec/candidates.js','./spec/sidebar.js'*/],
   //  main: ['./spec/reg.js','./spec/check-inbox.js','./spec/activate.js','./spec/login.js','./spec/agency_prefill.js','./spec/candidates.js'],
-    remote: ['./spec/dummy_data.js','./spec/login.js','./spec/check_agency.js'],
+    remote: ['./spec/dummy_data.js','./spec/login.js','./spec/search_current_candidate.js','./spec/candidates.js'],
     dummy: ['./spec/dummy_data.js','./spec/login.js','./spec/search_current_candidate.js','./spec/sidebar.js']
   },
 
