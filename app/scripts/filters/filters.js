0cf1e399dd66e363f31e6a601238baccc7b4f629
Ahmed Hashem <ahashem302@gmail.com>
2015-03-25 15:35:15 +0200

    completed statutory rates - 0001175

--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -6,73 +6,87 @@
  * @description
  * # fromvaild
  */
-angular.module('origApp.filters', [])
-        .filter('capitalize', function() {
-          return function(input) {
+ angular.module('origApp.filters', [])
+ .filter('capitalize', function() {
+  return function(input) {
             // input will be the string we pass in
             if (input){
               return input[0].toUpperCase() + input.slice(1);
             }
           };
         })
-        .filter('formatDate', function() {
-          return function(input, format) {
-            format = format || 'DD/MM/YYYY HH:mm:ss';
-            if (input === undefined) {
-              return input;
-            }
-            return moment(input).format(format);
-          };
-        })
+ .filter('formatDate', function() {
+  return function(input, format) {
+    format = format || 'DD/MM/YYYY HH:mm:ss';
+    if (input === undefined) {
+      return input;
+    }
+    return moment(input).format(format);
+  };
+})
 
-        .filter('dateRange', function() {
-          return function(input) {
-            console.log(input.validFrom);
-            return input;
-          };
-        })
+ .filter('validDate', function() {
+  return function(items, reverse) {
+    var filtered = [];
+    var today = new Date();
+    angular.forEach(items, function(item) {
+      var dateFrom = new Date(item.validFrom);
+      var dateTo = new Date(item.validTo);
+      if(!reverse){
+        if(dateFrom.getTime() <= today.getTime() && dateTo.getTime() >= today.getTime()) {
+          filtered.push(item);
+        }
+      } else {
+        if(dateTo.getTime() < today.getTime()) {
+          filtered.push(item);
+        }
+      }
+    });
+    return filtered;
+  };
+})
 
-        .filter('formatNumber', function($filter) {
-          function doFilter(input) {
-            if (input === null) {
-              return '';
-            }
-            var num = (input >= 0) ? $filter('number')(input, 2) : '(' + $filter('number')(Math.abs(input), 2) + ')';
-            num = (num === '()' ? '0.00' : num);
-            var parts = num.split('.');
-            return parts[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '.' + parts[1];
-          }
-          return function(input, fields) {
-            if (input === undefined) {
-              return input;
-            }
-            if (fields && typeof fields === 'object') {
-              _.each(input, function(inp) {
-                _.each(fields, function(field) {
-                  inp[field] = doFilter(inp[field]);
-                });
-              });
-            }
-            else if (fields && typeof fields === 'string') {
-              _.each(input, function(inp) {
-                inp[fields] = doFilter(inp[fields]);
-              });
-            }
-            else {
-              input = doFilter(input);
-            }
-            return input;
-          };
-        })
-        .filter('rowsSum', function() {
-          return function(input, field) {
-            if (input === undefined) {
-              return input;
-            }
-            var sum = 0;
-            _.each(input, function(inp) {
-              sum += isNaN(inp[field]*1) ? 0 : inp[field]*1;
-            });
-            return sum;
-          };
+ .filter('formatNumber', function($filter) {
+  function doFilter(input) {
+    if (input === null) {
+      return '';
+    }
+    var num = (input >= 0) ? $filter('number')(input, 2) : '(' + $filter('number')(Math.abs(input), 2) + ')';
+    num = (num === '()' ? '0.00' : num);
+    var parts = num.split('.');
+    return parts[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '.' + parts[1];
+  }
+  return function(input, fields) {
+    if (input === undefined) {
+      return input;
+    }
+    if (fields && typeof fields === 'object') {
+      _.each(input, function(inp) {
+        _.each(fields, function(field) {
+          inp[field] = doFilter(inp[field]);
         });
+      });
+    }
+    else if (fields && typeof fields === 'string') {
+      _.each(input, function(inp) {
+        inp[fields] = doFilter(inp[fields]);
+      });
+    }
+    else {
+      input = doFilter(input);
+    }
+    return input;
+  };
+})
+ .filter('rowsSum', function() {
+  return function(input, field) {
+    if (input === undefined) {
+      return input;
+    }
+    var sum = 0;
+    _.each(input, function(inp) {
+      sum += isNaN(inp[field]*1) ? 0 : inp[field]*1;
+    });
+    return sum;
+  };
+});
