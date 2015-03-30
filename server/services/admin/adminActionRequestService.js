034aed1e1a19554678e863c0ee193befd918228d
ishwor-mis <ishwor@makeitsimple.info>
2015-03-30 10:27:31 +0545

    action request routes,controller and services

--- server/services/admin/adminActionRequestService.js
+++ server/services/admin/adminActionRequestService.js
@@ -7,6 +7,7 @@ module.exports=function(dbs){
 	var candidateCommonService=require('../candidatecommonservice')(db);
 	var timesheetService=require('../timesheetService')(db);
 	var payrollService=require('../payrollService')(db);
+	var systemService=require('../systemservice')(db);
 	var enums=require('../../utils/enums');
 	var _=require('lodash');
 	var service={};
@@ -34,82 +35,76 @@ module.exports=function(dbs){
 		});
 	};
 
-	service.getActionRequestPayments=function(id,request){
-		var maxWeeks=0;
+	service.getActionRequestPayments=function(id,request,payType){
 		console.log('request is ');
 		console.log(request);
-		var dateInformed=request.dateInformed;
-		var startDate=moment(request.startDate);
-		var endDate=moment(request.endDate);
-		console.log('startDate:  '+request.startDate);
-		console.log('endDate:  '+request.endDate);
-		var weeklyPay=88.45;
-		var perDayPay=weeklyPay/7;
-		var workingDaysPerWeek=5;
 
 		return Q.Promise(function(resolve,reject){
-			return candidateCommonService.getUser(id)
-					.then(function(user){
+			return Q.all([candidateCommonService.getUser(id),systemService.getSystem()])
+					.spread(function(user,system){
 
 						if(!user){
 							reject({result:false,message:'User is not found.'});
 							return;
 						}
-						var payFrequency=user.worker.payrollTax.payFrequency;
-						var qualifyingDays=0,waitingDays=0,totalNumberOfDays;
-						waitingDays=0;//TODO: work out waiting days
-						var weeks=[];
-						totalNumberOfDays=endDate.diff(startDate,'days')+1;
-						console.log('totalNumberOfDays: '+totalNumberOfDays);
-						qualifyingDays=totalNumberOfDays-waitingDays;
-						console.log('qualifyingDays: '+qualifyingDays);
-						var requestStartDays=startDate.clone().add(waitingDays,'days');
-						console.log('requestStartDays: '+requestStartDays.toISOString());
-						var weekIndex=-1;
-						for(var i=0;i<totalNumberOfDays;i++){
 
-							var dt=requestStartDays.clone().add(i,'days');
-							console.log('dt: '+dt.toISOString());
-							var dayOfWeek=dt.day();//0=sunday , 6=saturday
-							console.log('current week index '+weekIndex);
-							console.log('dayOfWeek   '+dayOfWeek);
-							if(weekIndex>0){
-								console.log('weeks[weekIndex-1].length    '+ weeks[weekIndex-1].days.length);	
-							}
-							if(maxWeeks>0 && weekIndex>(maxWeeks-1)){
-								break;
-							}
+
+
+						var options={weeklyPay:0};
+						var currentRate;
+						
+						if(payType==='ssp'){
 							
-							if(weekIndex<0 || (dayOfWeek===1 && (weekIndex<=0 || weeks[weekIndex-1].days.length>0))) {
-								weekIndex+=1;
-								console.log('weekIndex: '+weekIndex);
-							}
-
-							weeks[weekIndex]=weeks[weekIndex]||{'days':[],amount:0,weekMonthStartDate:null,weekNumber:-1,monthNumber:-1};
-
-							if(dayOfWeek>0 && dayOfWeek<6){
-								weeks[weekIndex].days.push(utils.getDateValue(dt.toDate()));
-							}
-
-							if(weeks[weekIndex].days.length===1){
-								weeks[weekIndex].weekMonthStartDate=utils.getDateValue(moment(weeks[weekIndex].days[0]).day(1).toDate());
-								console.log('week start date '+weeks[weekIndex].weekMonthStartDate.toISOString());
-							}
-							// var nextDayOfWeek=requestStartDays.add(i+1,'days').day();
-
 							
+							var currentRate=utils.getStatutoryValue('sspRate',system,new Date());
+
+							options.weeklyPay=currentRate?currentRate.amount:0;
 						}
-						console.log('week calculations done');
-						console.log('total weeks ');
-						console.log(weeks);
-						console.log('weeks[0].weekMonthStartDate.toDate()   '+weeks[0].weekMonthStartDate );
-						console.log('weeks[weeks.length-1].weekMonthStartDate.toDate()    '+weeks[weeks.length-1].weekMonthStartDate);
-						return applyWeekMonthNumbers(user,weeks)
-						.then(function(weeks){
-							resolve({result:true,objects:weeks});
-						});
+						else if(payType==='smp'){
+							
+							currentRate=utils.getStatutoryValue('smpRate',system,new Date());
+							options.weeklyPay=currentRate?currentRate.amount:0;
+						}
+						else if(payType==='spp'){
+							
+							
+							currentRate=utils.getStatutoryValue('sppRate',system,new Date());
+							options.weeklyPay=currentRate?currentRate.amount:0;
+						}
+
+						console.log('----currentRate----');
+						console.log(currentRate);
+
+						return calculateByWeeks(user,request,options)
+							.then(function(response){
+								console.log('week calculations done');
+								if(!response.result){
+									reject(response);
+									return false;
+								}
+								var records=response.objects;
+
+								console.log('total records ');
+								console.log(records);
+								console.log('records[0].periodStartDate.toDate()   '+records[0].periodStartDate );
+								console.log('records[records.length-1].periodStartDate.toDate()    '+records[records.length-1].periodStartDate);
+								return applyPeriodNumbers(user,records,response.options)
+								.then(function(){
+									resolve({result:true,objects:records});
+								});
+							})
+							.catch(function(err){
+								console.log('error');
+								console.log(err);
+							});
+						
+						
 
 
+					})
+					.catch(function(err){
+						console.log('errrr');
+						console.log(err);
 					});
 		})
 		.then(null,function(err){
@@ -118,34 +113,105 @@ module.exports=function(dbs){
 		});
 	};
 
-	function applyWeekMonthNumbers(user,weeks){
-		var q=db.TaxTable.find().gte('startDate',weeks[0].weekMonthStartDate).lte('startDate',weeks[weeks.length-1].weekMonthStartDate);
+	function calculateByWeeks(user,request,options){
+		
+		var maxPeriods=request.maxPeriods||0;
+		console.log('max periods.... '+maxPeriods);
+		
+		var dateInformed=request.dateInformed;
+		
+		var startDate=moment(request.startDate);
+		console.log('startDate:  '+request.startDate);
+		var endDate=request.endDate?moment(request.endDate):null;
+		
+		console.log('endDate:  '+request.endDate);
+		var weeklyPay=options.weeklyPay//options.weeklyPay=88.45;
+		var perDayPay=weeklyPay/7;
+		var workingDaysPerWeek=5;
+	
+		// var payFrequency=user.payrollTax.payFrequency||enums.payFrequency.Weekly;
+		return Q.Promise(function(resolve,reject){
+			var payFrequency=user.worker.payrollTax.payFrequency;
+			var waitingDays=0;
+			waitingDays=0;//TODO: work out waiting days
+			var weeks=[];
+			// totalNumberOfDays=endDate.diff(startDate,'days')+1;
+			// console.log('totalNumberOfDays: '+totalNumberOfDays);
+			
+			var requestStartDays=startDate.clone().add(waitingDays,'days');
+			console.log('requestStartDays: '+requestStartDays.toISOString());
+			var weekIndex=-1;
+			var nextStartDate=requestStartDays.clone();
+			console.log('nextStartDate '+nextStartDate.toISOString());
+			var i=0;
+			while(true){
+				console.log('current nextStartDate is '+nextStartDate.toISOString());
+				if(maxPeriods>0 && maxPeriods<(i+1)){
+					console.log('max periods '+ maxPeriods + ' Exceeded');
+					break;
+				}
+				if(endDate && nextStartDate>endDate){
+					console.log('nextStartDate '+ nextStartDate.toISOString() + ' Exceeded from endDate '+endDate.toISOString());
+					break;
+				}
+				var thisPeriodLastDate=nextStartDate.clone().day(5);
+				
+				var thisPeriodEndDate=endDate && thisPeriodLastDate.diff(endDate,'days')>0?endDate.clone():thisPeriodLastDate.clone();
+				var noOfDays=thisPeriodEndDate.diff(nextStartDate,'days')+1;
+				console.log('noOfDays '+noOfDays);
+				weeks[i]={'days':[],amount:0,periodStartDate:null,weekNumber:-1,monthNumber:-1};
+				weeks[i].noOfDays=noOfDays;
+
+				weeks[i].periodStartDate=utils.getDateValue(nextStartDate.clone().day(1).toDate());
+
+				nextStartDate=nextStartDate.clone().add(7,'days').day(1);
+				i++;
+			}
+			
+
+			_.forEach(weeks,function(week,idx){
+					
+				week.amount=perDayPay * week.noOfDays;
+			});
+
+			resolve({result:true,objects:weeks,options:{perDayPay:perDayPay}});
+		})
+.then(null,function(err){
+	console.log('erererere');
+	console.log(err);
+});
+	}
+
+	function applyPeriodNumbers(user,records,options){
+		var perDayPay=options.perDayPay;
+
+		var q=db.TaxTable.find().gte('startDate',records[0].periodStartDate).lte('startDate',records[records.length-1].periodStartDate);
 		console.log('query formed');
 		return Q.Promise(function(resolve,reject){
 			return q.exec(function(err,taxTables){
 				console.log('taxTables');
 				console.log(taxTables.length);
-				_.forEach(weeks,function(week,idx){
+				_.forEach(records,function(week,idx){
 					var taxTable=_.first(_.filter(taxTables,function(tbl){
-						console.log('comparing dates : ');
-						console.log(week.weekMonthStartDate+ '  ,  '+tbl.startDate);
+						//console.log('comparing dates : ');
+						//console.log(week.periodStartDate+ '  ,  '+tbl.startDate);
 						// var tblMomenentDate=moment(tbl);
-						var ret= utils.areDateEqual(week.weekMonthStartDate,tbl.startDate);
-						console.log(ret);
+						var ret= utils.areDateEqual(week.periodStartDate,tbl.startDate);
+						//console.log(ret);
 						return ret;
 					}));
 
 					if(taxTable){
-						console.log('taxTable found ');
-						console.log(taxTable);
+						// console.log('taxTable found ');
+						// console.log(taxTable);
 						week.weekNumber=taxTable.weekNumber;
 						week.monthNumber=taxTable.monthNumber;
 					}
 
-					week.amount=perDayPay * week.days.length;
+					// week.amount=perDayPay * week.days.length;
 				});
 
-				resolve(weeks);
+				resolve(records);
 			});
 		});
 		
