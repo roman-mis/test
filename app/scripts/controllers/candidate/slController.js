c44a1cc998363b30d50e36966903282ccfb4acc9
bikrambasnet <bikram@makeitsimple.info>
2015-04-01 13:51:21 +0545

    student load

--- app/scripts/controllers/candidate/slController.js
+++ app/scripts/controllers/candidate/slController.js
@@ -18,4 +18,11 @@ angular.module('origApp.controllers')
         $scope.candidateInfo = data.data.object;
      //   $scope.fullname = ($scope.candidateInfo.firstName + ' ' + $scope.candidateInfo.lastName);
     }, function(err) {});
+    $scope.submitAction=function(values){
+
+       HttpResource.model('actionrequests/' + $scope.candidateId+'/studentloan').create(values).post().then(function(response) {
+                  $scope.sl={};
+
+                });
+    };
 });
