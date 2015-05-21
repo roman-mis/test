2d6168331fb910b9679508e87d847b16039ef71b
CodeTrol <0x80000000h@gmail.com>
2015-05-22 01:12:10 +0300

    expense wizard update

--- test/e2e/spec/candidate/sidebar/expense_wizard.js
+++ test/e2e/spec/candidate/sidebar/expense_wizard.js
@@ -30,7 +30,7 @@ describe('checking expense wizard', function () {
 
   var days = element(by.model('addData.date'));
 
-  it('adding dates', function () {
+  it('adding date', function() {
     var startH = element(by.model('addData.startHours'));
     var startM = element.all(by.model('addData.startMins'));
     var endH = element.all(by.model('addData.endHours'));
@@ -40,65 +40,57 @@ describe('checking expense wizard', function () {
     helper.selectSimpleSelect(startM, 2);
     helper.selectSimpleSelect(endH, 17);
     helper.selectSimpleSelect(endM, 3);
+    
     element(by.css('[ng-click="add()"]')).click();
+    
+    element.all(by.repeater('item in expenseData.times')).each(function(row){
+		row.all(by.tagName('td')).then(function (cols) {
 
-
-    var rows = element.all(by.repeater('item in expenseData.times'));
-    rows.first().then(function (row) {
-      var rowElems = row.all(by.tagName('td'));
-      rowElems.then(function (cols) {
-
-        expect(cols[0].getText()).toContain(days.all(by.tagName('option')).get(2).getText());
-        cols[1].getText().then(function (str) {
-          expect(str.split(':')[0] + ' ').toContain(startH.all(by.tagName('option')).get(10).getText());
-          expect(str.split(':')[1] + ' ').toContain(startM.all(by.tagName('option')).get(2).getText());
-        });
-        cols[2].getText().then(function (str) {
-          expect(str.split(':')[0] + ' ').toContain(endH.all(by.tagName('option')).get(17).getText());
-          expect(str.split(':')[1] + ' ').toContain(endM.all(by.tagName('option')).get(3).getText());
-        });
-        okBtn.click();
-      });
-    });
-
+			expect(cols[0].getText()).toContain(days.all(by.tagName('option')).get(2).getText());
+			cols[1].getText().then(function (str) {
+				expect(str.split(':')[0] + ' ').toContain(startH.all(by.tagName('option')).get(10).getText());
+				expect(str.split(':')[1] + ' ').toContain(startM.all(by.tagName('option')).get(2).getText());
+			});
+			cols[2].getText().then(function (str) {
+				expect(str.split(':')[0] + ' ').toContain(endH.all(by.tagName('option')).get(17).getText());
+				expect(str.split(':')[1] + ' ').toContain(endM.all(by.tagName('option')).get(3).getText());
+			});
+			okBtn.click(); 
+		});
+	});           
+    
   });
-
-  it('adding location', function () {
-
-    element(by.model('addData.code')).sendKeys('E20 2BB');
+  
+  it('adding location', function(){
+	element(by.model('addData.code')).sendKeys('E20 2BB');
     helper.selectSimpleSelect(days, 2);
     element(by.css('[ng-click="add()"]')).click();
-
-    var rows = element.all(by.repeater('item in expenseData.postCodes'));
-    rows.first().then(function (row) {
-      var rowElems = row.all(by.tagName('td'));
-      rowElems.then(function (cols) {
+    
+    element.all(by.repeater('item in expenseData.postCodes')).each(function (row) {
+      row.all(by.tagName('td')).then(function (cols) {
         expect(cols[0].getText()).toContain(days.all(by.tagName('option')).get(2).getText());
         expect(cols[1].getText()).toContain('E20 2BB');
         okBtn.click();
       });
-    });
+    });        
   });
-
+  
   it('transport dialog', function () {
 
     helper.selectSimpleSelect(days, 2);
     helper.selectSimpleSelect(element(by.model('addData.type')), 1);
     element.all(by.model('addData.mileage')).get(0).sendKeys('100');
     element(by.css('[ng-click="add()"]')).click();
-
-    var rows = element.all(by.repeater('item in expenseData.transports'));
-    rows.first().then(function (row) {
-      var rowElems = row.all(by.tagName('td'));
-      rowElems.then(function (cols) {
+    
+    element.all(by.repeater('item in expenseData.transports')).each(function (row) {
+      row.all(by.tagName('td')).then(function (cols) {
         expect(cols[0].getText()).toContain(days.all(by.tagName('option')).get(2).getText());
         expect(cols[1].getText()).toContain(element(by.model('addData.type')).all(by.tagName('option')).get(1).getText());
         okBtn.click();
       });
-    });
-
+    });    
   });
-
+  
   it('vehicle info dialog', function () { //arbitrariry field
     element(by.model('vehicle.fuelType')).isPresent().then(function (bool) {
       if (bool) {
@@ -119,39 +111,37 @@ describe('checking expense wizard', function () {
     helper.selectSimpleSelect(days, 2);
     helper.selectSimpleSelect(element(by.model('addData.type')), 1);
     element(by.css('[ng-click="add()"]')).click();
-
-    var rows = element.all(by.repeater('item in expenseData.subsistences'));
-    rows.first().then(function (row) {
-      var rowElems = row.all(by.tagName('td'));
-      rowElems.then(function (cols) {
+    
+    element.all(by.repeater('item in expenseData.subsistences')).each(function (row) {
+      row.all(by.tagName('td')).then(function (cols) {
         expect(cols[0].getText()).toContain(days.all(by.tagName('option')).get(2).getText());
    //     expect(cols[1].getText()).toContain(element(by.model('addData.type')).all(by.tagName('option')).get(1).getText());
         okBtn.click();
       });
     });
-
+    
   });
-
+  
   it('Document other expenses', function () {
 
     helper.selectSimpleSelect(days, 2);
     helper.selectSimpleSelect(element(by.model('addData.type')), 1);
     element(by.model('addData.cost')).sendKeys('100');
     element(by.css('[ng-click="add()"]')).click();
-
-    var rows = element.all(by.repeater('item in expenseData.others'));
-    rows.first().then(function (row) {
-      var rowElems = row.all(by.tagName('td'));
-      rowElems.then(function (cols) {
+    
+    element.all(by.repeater('item in expenseData.others')).each(function (row) {
+	  row.all(by.tagName('td')).count(function(n){ console.log(n); });
+      row.all(by.tagName('td')).then(function (cols) {
         expect(cols[0].getText()).toContain(days.all(by.tagName('option')).get(2).getText());
      //   expect(cols[1].getText()).toContain(element(by.model('addData.type')).all(by.tagName('option')).get(1).getText());
-        expect(cols[3].getText()).toContain('100');
+        expect(cols[2].getText()).toContain('100');
         okBtn.click();
       });
     });
-
+    
+    
   });
-
+  
   it('Receipts dialog', function () {
     okBtn.click();
   });
@@ -165,11 +155,11 @@ describe('checking expense wizard', function () {
   });
 
   it('thank you screen', function () {
-    expect(element(by.model('expenseData.claimReference')).getAttribute('value')).toBeTruthy();
+	//expect(element(by.model('expenseData.claimReference')).getAttribute('value')).toBeTruthy();
+	expect(element(by.binding('expenseData.claimReference')).getText()).toBeTruthy();
     element(by.css('[ng-click="cancel()"]')).click();
     expect($('.modal-content').isPresent()).toBeFalsy();
   });
-
 });
 
 
