a7d24acdac773e102d60b5805b5163d3f3190d6c
CodeTrol <0x80000000h@gmail.com>
2015-05-29 02:43:21 +0300

    Action Request test v0.0.1 (unstable)

---
+++ test/e2e/spec/candidate/sidebar/action_requests.js
@@ -0,0 +1,78 @@
+var path = require('path');
+
+describe('Checking Action Requests', function(){
+	function toggleOnActionRequestMenu()
+	{
+		var el = element.all(by.css('#left-menu [collapse="isActionRequestCollapsed"]')).first();
+		el.getAttribute('class').then(function(classStr){
+			var classes = classStr.split(/ +/g);
+			if(classes.indexOf('in') == -1){
+				//! 'toggle on "Action Request" buttons'
+				element(by.css('#left-menu [ng-if="!permissions.rightToolBar.actionRequest.hide"] [ng-click="toggle()"]')).click();
+				//! 'wait for "toggle" animation'
+				browser.sleep(1000);
+				el.getAttribute('class').then(function(classStr){
+					var isToggledOn = classStr.split(/ +/g).indexOf('in') == -1;
+					//! 'check if toggled:', isToggledOn
+					expect(isToggledOn).toBeTruthy();
+				});
+			}
+			else {
+				//! '"Action Request" buttons are already visible'
+			}
+		});
+	}
+	
+	it('should make Student Loan (SL) requests', function(){
+		toggleOnActionRequestMenu();
+		//! 'Opening dialog'
+		element(by.css('[ng-click="openSl()"]')).click();
+		browser.sleep(1000);
+		expect(element(by.css('.modal-content')).isPresent()).toBeTruthy();
+		
+		//! 'Clicking on checkboxes'
+		element(by.model('studentLoan.haveLoan')).click();
+		element(by.model('studentLoan.payDirectly')).click();
+		
+		//! 'Saving changes by clicking on button'
+		element(by.css('.modal-footer')).all(by.buttonText('Save')).first().click();
+		browser.sleep(50);
+		expect(element(by.css('.alert-success')).isPresent()).toBeTruthy();
+		browser.sleep(500);
+		expect(element(by.css('.modal-content')).isPresent()).toBeFalsy();
+	});
+	
+	it('should make P45 requests', function(){
+		var picturePath = path.normalize(__dirname + '../../../../res/test.jpg');
+		
+		toggleOnActionRequestMenu();
+		//! 'Opening dialog'
+		element(by.css('[ng-click="openP45()"]')).click();
+		expect(element(by.css('.modal-content')).isPresent()).toBeTruthy();
+		
+		//! 'Typing request date'
+		element.all(by.model('dateRequested')).all(by.css('input')).first().clear().sendKeys('01/01/2015');
+		//! 'Typing leaving date'
+		element.all(by.model('leavingDate')).all(by.css('input')).first().clear().sendKeys('07/01/2015');
+		
+		//! 'Temporaly make input[type="file"] displayable'
+		browser.executeScript('$("#uploadBtn").css("display","inline-block")');
+		//! 'Using picture at "'+picturePath+'"'
+		element(by.css('#uploadBtn')).sendKeys(picturePath);		
+		browser.executeScript('$("#uploadBtn").css("display","none")');
+		//! 'Clicking on "Upload" button'
+		element(by.css('[ng-click="uploadFile()"]')).click();
+		browser.wait(function(){
+			return element(by.partialButtonText('Cancel upload')).isPresent().then(function(b){
+				return !b;
+			});
+		});
+		
+		//! 'Clicking on "Save" button'
+		element(by.css('.modal-footer')).all(by.buttonText('Save')).first().click();
+		browser.sleep(100);
+		expect(element(by.css('.alert-success')).isPresent()).toBeTruthy();
+		browser.sleep(500);
+		expect(element(by.css('.modal-content')).isPresent()).toBeFalsy();
+	});
+}); 
