a57c321ed8353680639d00979b18f8fbd4725167
CodeTrol <0x80000000h@gmail.com>
2015-06-01 09:37:18 +0300

    added more checks for 'candidates / sidebar / timesheet'

--- test/e2e/spec/candidate/sidebar/timesheet.js
+++ test/e2e/spec/candidate/sidebar/timesheet.js
@@ -10,6 +10,8 @@ describe('Checking Timesheet', function(){
 	});
 	
 	it('adding timesheet', function(){
+		//! 'selecting agency'
+		helper.selectSimpleSelect(element(by.model('saveAgency')), 0);
 		//! 'clicking on add date'
 		element.all(by.css('[ng-click="clicked = true"]')).get(1).click();
 		//! 'selecting week'
@@ -24,8 +26,43 @@ describe('Checking Timesheet', function(){
 		element(by.model('elements.payRate')).clear().sendKeys('8');
 		//! 'typing description'
 		element(by.model('userDescription')).clear().sendKeys('Timesheet description');
+		
+		var rate, description, unit, payRate;
+		
+		element(by.model('saveRate')).getAttribute('value').then(function(index){
+			element(by.model('saveRate')).all(by.tagName('option')).get(index|0).getText().then(function(value){
+				rate = value;
+			});
+		});
+		
+		element(by.model('userDescription')).getAttribute('value').then(function(value){
+			description = value;
+		});
+		
+		element(by.model('elements.unit')).getAttribute('value').then(function(value){
+			unit = value;
+		});
+		
+		element(by.model('elements.payRate')).getAttribute('value').then(function(value){
+			payRate = value;
+		});
+		
 		//! 'inserting'
 		element(by.css('.modal-window .modal-body [ng-click="populateTable()"]')).click();
+		
+		//! 'checking input values'
+		element.all(by.repeater('element in finalElements')).then(function(rows){
+			var row = rows[0];
+			//! ' checking rate'
+			expect(row.all(by.css('td')).get(0).getText()).toContain(rate);
+			//! ' checking description'
+			expect(row.all(by.css('td')).get(1).getText()).toContain(description);
+			//! ' checking unit'
+			expect(row.all(by.css('td')).get(2).getText()).toContain(unit);
+			//! ' checking payRate'
+			expect(row.all(by.css('td')).get(3).getText()).toContain(payRate);
+		});
+		
 		//! 'uploading file'
 		browser.executeScript("$('#upload_file').css('display', 'block');");
 		element(by.css('#upload_file')).sendKeys(picturePath);
