814b3fcae1b5bd25bd53c140e776d52c5c5535df
CodeTrol <0x80000000h@gmail.com>
2015-06-03 00:36:01 +0300

    updated 'Agencies / Home Tab'

--- test/e2e/spec/agency/check_agency.js
+++ test/e2e/spec/agency/check_agency.js
@@ -11,7 +11,7 @@ describe('Getting first agency properties', function () {
   });
 
   it('selecting first agency in the list', function () {
-    element(by.repeater('row in options.data').row(0)).element(by.css('td>a')).click();
+    element(by.repeater('row in options.data').row(0)).element(by.css('td:first-child')).click();
     browser.wait(function () {
       return browser.getCurrentUrl().then(function (url) {
         return (url.match(/agencies\/.{24}/g));
@@ -36,7 +36,7 @@ describe('Getting first agency properties', function () {
 
 
   it('checking tabs consistency', function () {
-
+	//! 'checking link names'
     expect(links.get(0).getText()).toBe('Home');
     expect(links.get(1).getText()).toBe('Payroll');
     expect(links.get(2).getText()).toBe('Consultants');
@@ -44,33 +44,39 @@ describe('Getting first agency properties', function () {
     expect(links.get(4).getText()).toBe('Sales');
     expect(links.get(5).getText()).toBe('History');
     expect(links.get(6).getText()).toBe('Other');
-
+	//! 'checking Home url address'
     links.get(0).click();
     checkTabUrl('');
+    //! 'checking Payroll url address'
     links.get(1).click();
     checkTabUrl('payroll');
+    //! 'checking Consultants url address'
     links.get(2).click();
     checkTabUrl('consultants');
+    //! 'checking Candidates url address'
     links.get(3).click();
     checkTabUrl('candidates');
+    //! 'checking Sales url address'
     links.get(4).click();
     checkTabUrl('sales');
+    //! 'checking History url address'
     links.get(5).click();
     checkTabUrl('history');
+    //! 'checking Other url address'
     links.get(6).click();
     checkTabUrl('other');
 
   });
-
+/*
   it('selecting first agency in the list', function () {
 
   });
-
+*/
 });
 
 describe('Editing home tab', function () {
 
-  var inputs = element.all(by.css('.modal-content input'));
+  var inputs;
 
   var number = helper.getDefaultNumber();
   var saveBtn = $('.modal-content [ng-click="ok()"]');
@@ -79,60 +85,121 @@ describe('Editing home tab', function () {
 
     links.get(0).click();
 
+	[0,1].forEach(function(status){
+		//! 'open Edit Agency dialog'
+		$('[ng-click="openAgencyEdit()"]').click();
+    
+		browser.wait(function(){
+			inputs = {};
+			return true;
+		});
+    
+		element.all(by.css('.modal-content [ng-model^="data."]')).each(function(field){		
+			
+			field.getAttribute('ng-model').then(function(model){
+				inputs[model.replace(/^data\./, '')] = field;
+			});
+		});
+    
+		browser.wait(function(){    
+			//! ' selecting status'
+			helper.selectSelector(inputs.status, status);
+		
+			//! ' typing agency name'
+			inputs.name.clear().sendKeys('Agency Name_' + number);		
+			//! ' typing address #1'
+			inputs.address1.clear().sendKeys('Address1_' + number);		
+			//! ' typing address #2'
+			inputs.address2.clear().sendKeys('Address2_' + number);		
+			//! ' typing address #3'
+			inputs.address3.clear().sendKeys('Address3_' + number);
+			//! ' typing town'
+			inputs.town.clear().sendKeys('Town_' + number);		
+			//! ' selecting country'
+			helper.selectSelector(inputs.country, 0);
+			//! ' typing post code'
+			inputs.postCode.clear().sendKeys('E22 2EE');
+			if(status == 1){
+				//! 'typing registration number'
+				inputs.companyRegNo.clear().sendKeys('23'+number);
+				//! 'typing VAT number'
+				inputs.companyVatNo.clear().sendKeys('123'+number);
+			}
+			return true;
+		});
+		//! ' clicking on "save" button'
+		expect(saveBtn.isEnabled()).toBeTruthy();
+	
+		saveBtn.click();
+		
+		browser.refresh();
+		
+		var addressInformationLabels = element.all(by.css('.meta'));
+		
+		//! 'checking if data is saved'
+		expect(addressInformationLabels.get(0).getText()).toBe('Agency Name_' + number);
+		expect(addressInformationLabels.get(1).getText()).toBe('Address1_' + number);
+		expect(addressInformationLabels.get(2).getText()).toBe('Address2_' + number);
+		expect(addressInformationLabels.get(3).getText()).toBe('Address3_' + number);
+		expect(addressInformationLabels.get(4).getText()).toBe('Town_' + number);
+		expect(addressInformationLabels.get(5).getText()).toBe('United Kingdom');
+		expect(addressInformationLabels.get(6).getText()).toBe('E22 2EE');
+		if(status == 1){
+			expect(addressInformationLabels.get(7).getText()).toBe('23'+number);
+			expect(addressInformationLabels.get(8).getText()).toBe('123'+number);
+		}
+		
+	});
 
-    $('[ng-click="openAgencyEdit()"]').click();
-    inputs.get(0).clear().sendKeys('Agency Name_' + number);
-    inputs.get(1).clear().sendKeys('Address1_' + number);
-    inputs.get(2).clear().sendKeys('Address2_' + number);
-    inputs.get(3).clear().sendKeys('Address3_' + number);
-    inputs.get(4).clear().sendKeys('Town_' + number);
-    helper.selectSelector($('.modal-content .select2'), 0);
-    inputs.get(7).clear().sendKeys('E22 2EE');
-    inputs.get(8).clear().sendKeys(number);
-    inputs.get(9).clear().sendKeys(number);
-    saveBtn.click();
 
-
-    $('[ng-click="openContactEdit()"]').click();
-    inputs.get(0).clear().sendKeys(number);
-    inputs.get(1).clear().sendKeys(number);
-    inputs.get(2).clear().sendKeys(number);
-    inputs.get(3).clear().sendKeys('facebook.com/' + number);
-    inputs.get(4).clear().sendKeys('linkedin.com/' + number);
-    inputs.get(5).clear().sendKeys('website.com');
-    inputs.get(6).clear().sendKeys('super@email.com');
-    saveBtn.click();
-
-    var labels = element.all(by.css('.meta'));
-
-    expect(labels.get(0).getText()).toBe('Agency Name_' + number);
-    expect(labels.get(1).getText()).toBe('Address1_' + number);
-    expect(labels.get(2).getText()).toBe('Address2_' + number);
-    expect(labels.get(3).getText()).toBe('Address3_' + number);
-    expect(labels.get(4).getText()).toBe('Town_' + number);
-    expect(labels.get(5).getText()).toBe('United Kingdom');
-    expect(labels.get(6).getText()).toBe('E22 2EE');
-    expect(labels.get(7).getText()).toBe(number);
-    expect(labels.get(8).getText()).toBe(number);
-    expect(labels.get(9).getText()).toBe(number);
-    expect(labels.get(10).getText()).toBe(number);
-    expect(labels.get(11).getText()).toBe(number);
-    expect(labels.get(12).getText()).toBe('facebook.com/' + number);
-    expect(labels.get(13).getText()).toBe('linkedin.com/' + number);
-    expect(labels.get(14).getText()).toBe('website.com');
-    expect(labels.get(15).getText()).toBe('super@email.com');
-
+	//! 'open Edit Contacts dialog'
+	$('[ng-click="openContactEdit()"]').click();
+	
+	browser.wait(function(){
+		inputs = element.all(by.css('.modal-content input[ng-model]'));
+		//! ' populating data'
+		inputs.get(0).clear().sendKeys('02012'+number);
+		inputs.get(1).clear().sendKeys('02012'+number);
+		inputs.get(2).clear().sendKeys('02012'+number);
+		inputs.get(3).clear().sendKeys('facebook.com/' + number);
+		inputs.get(4).clear().sendKeys('linkedin.com/' + number);
+		inputs.get(5).clear().sendKeys('http://website.com');
+		inputs.get(6).clear().sendKeys('super@email.com');
+		
+		return true;
+	});
+	
+	//! ' clicking on "save" button'
+	expect(saveBtn.isEnabled()).toBeTruthy();
+	//browser.pause();
+	saveBtn.click();
+	
+	browser.refresh();   
+    
+    //! ' check if data is saved'
+    var contactInformationLabels = element.all(by.css('.meta-o'));
+    
+    expect(contactInformationLabels.get(0).getText()).toBe('02012'+number);
+    expect(contactInformationLabels.get(1).getText()).toBe('02012'+number);
+    expect(contactInformationLabels.get(2).getText()).toBe('02012'+number);
+    expect(contactInformationLabels.get(3).getText()).toBe('facebook.com/' + number);
+    expect(contactInformationLabels.get(4).getText()).toBe('linkedin.com/' + number);
+    expect(contactInformationLabels.get(5).getText()).toBe('http://website.com');
+    expect(contactInformationLabels.get(6).getText()).toBe('super@email.com');
+    
   });
 
 
 });
-
+/*
 describe('Editing consultants tab', function () {
 
   var number = helper.getDefaultNumber();
 
 
   it('add new branch', function () {
+	  
+	browser.refresh();
 
     links.get(2).click();
     $('[ng-click="openAgencyBranchModal(null)"]').click();
@@ -149,7 +216,7 @@ describe('Editing consultants tab', function () {
   });
 
   it('editing new branch', function () {
-
+    browser.refresh();
 
     $$('[ng-click="toggleOpen()"]').last().click();
     $$('[ng-click="openAgencyBranchModal(branch)"]').last().click();
@@ -166,7 +233,7 @@ describe('Editing consultants tab', function () {
   });
 
   it('editing new consultant', function () {
-
+    browser.refresh();
     $$('[ng-click="openAgencyConsultantModal(branch, null)"]').last().click();
 
     element(by.model('data.firstName')).clear().sendKeys('FirstName_'+number);
