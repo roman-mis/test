e5eb048308750640db0ce0c8564d51636eafe7bd
CodeTrol <0x80000000h@gmail.com>
2015-06-11 21:21:13 +0300

    'Candidates / Home tab' update

--- test/e2e/spec/candidate/home_tab.js
+++ test/e2e/spec/candidate/home_tab.js
@@ -1,6 +1,14 @@
 var links = $$('.table-view-main-content .tabs-wrapper .nav-tabs a[href^="/candidates/"]');
 
 describe('Candidate Home tab', function () {
+  var passportNumber = helper.getDefaultNumber();
+  
+  var expireDate = new Date(new Date().getTime() + 1000*60*60*24*305);
+  
+  var typingExpireDateStr = [expireDate.getMonth()+1, expireDate.getDate(), expireDate.getFullYear()].join('/');
+  
+  var readingExpireDateStr = [('0'+expireDate.getDate()).slice(-2), ('0'+(expireDate.getMonth()+1)).slice(-2), expireDate.getFullYear()].join('/');
+  
   it('Checking dialog data input',function(){
     helper.getByText(links, 'Home').click();
     $('[ng-click="editDetails()"]').click();
@@ -11,8 +19,16 @@ describe('Candidate Home tab', function () {
     element(by.model('candidate.middleName')).clear().sendKeys('MiddleName');
     element(by.model('candidate.lastName')).clear().sendKeys('LastName');
     helper.selectSelector(element(by.model('candidate.gender')),0);
+    
  //   element(by.css('[ng-model="candidate.birthDate"] [ng-model="ngModel"]')).clear().sendKeys('26/03/1987');
     helper.selectSelector(element(by.model('candidate.nationality')),0);
+    
+    element(by.model('candidate.passportNumber')).clear().sendKeys(passportNumber);
+    
+    
+    element(by.model('candidate.passportExpiryDate')).element(by.model('ngModel')).clear().sendKeys(typingExpireDateStr);
+	
+	element(by.model('candidate.drivingLicenceNumber')).clear().sendKeys('MORGA657054SM0IJ');
 
     $('[ng-click="saveCandidate()"]').click();
     browser.driver.switchTo().alert().accept();
@@ -31,6 +47,9 @@ describe('Candidate Home tab', function () {
     expect(labels.get(3).getText()).toBe('LastName');
     expect(labels.get(4).getText()).toBe('Male');
     expect(labels.get(6).getText()).toBe('United Kingdom');
+    expect(labels.get(7).getText()).toBe(passportNumber);
+	expect(labels.get(8).getText()).toBe(readingExpireDateStr);
+    expect(labels.get(9).getText()).toBe('MORGA657054SM0IJ');
 
   });
 });
