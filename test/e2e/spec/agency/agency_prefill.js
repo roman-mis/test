b805b1f6acfa2ef8b94d6a1aae10c2e199c8ff24
CodeTrol <0x80000000h@gmail.com>
2015-06-15 00:02:55 +0300

    'Agency / prefill' update

--- test/e2e/spec/agency/agency_prefill.js
+++ test/e2e/spec/agency/agency_prefill.js
@@ -13,22 +13,24 @@ describe('Checking agency for sufficient count', function() {
     });
   });
   it('count agencies in the table', function () {
-    var addBtn=$('[ng-click="openAddAgencyWin()"]');
+    var addBtn=$('[ng-click="addNewAgency()"]');
     var agenciesNames=['Sumertime agency','Woodoo agency','Kengoo Agency','Bogart Int','Slimshady Agency']
     var addAgency=function(i){
       addBtn.click();
       expect($('.modal-content').isDisplayed()).toBeTruthy();
-      element.all(by.repeater('field in fields')).get(0).element(by.css('input')).sendKeys(agenciesNames[i-1]);
-      element.all(by.repeater('field in fields')).get(1).element(by.css('input')).sendKeys('Super Address1_'+i);
-      element.all(by.repeater('field in fields')).get(2).element(by.css('input')).sendKeys('Super Address2_'+i);
-      element.all(by.repeater('field in fields')).get(3).element(by.css('input')).sendKeys('Super Address3_'+i);
-      element.all(by.repeater('field in fields')).get(4).element(by.css('input')).sendKeys('Super Town');
-      helper.selectSelector(element.all(by.repeater('field in fields')).get(5).element(by.css('.select2')),0);
-      element.all(by.repeater('field in fields')).get(6).element(by.css('input')).sendKeys('E20 2BB');
-      element.all(by.repeater('field in fields')).get(7).element(by.css('input')).sendKeys(number.substr(-5,5));
-      element.all(by.repeater('field in fields')).get(8).element(by.css('input')).sendKeys(number.substr(-6,6));
+      element(by.model('data.name')).sendKeys(agenciesNames[i-1]);
+      element(by.model('data.address1')).sendKeys('Super Address1_'+i);
+      element(by.model('data.address2')).sendKeys('Super Address2_'+i);
+      element(by.model('data.address3')).sendKeys('Super Address3_'+i);
+      element(by.model('data.town')).sendKeys('Super Town');
+      helper.selectSelector(element(by.model('data.country')),0);
+      element(by.model('data.postCode')).sendKeys('E20 2BB');
+      element(by.model('data.companyRegNo')).sendKeys((number+number).slice(-8));
+      element(by.model('data.companyVatNo')).sendKeys((number+number).slice(-9));
 
       $('[ng-click="ok()"]').click();
+      helper.alertAccept();
+      browser.sleep(1000);
       expect($('.modal-content').isPresent()).toBeFalsy();
     };
 
